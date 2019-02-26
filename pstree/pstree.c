ad584c3cde22ac09d91aacb9b1132bb7b372035b
tracer-nju <tracer@nju.edu.cn>
2019-02-26 16:03:31 +0800

    Static hostname: doowzs-eve          Icon name: computer-desktop            Chassis: desktop         Machine ID: a024f767cdeb40369ab1831e42d9d10c            Boot ID: 346c7d302d0e472c9befe7d2dfa2face   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.4.164chromium       Architecture: x86-64  16:03:31 up 1 day,  1:44,  4 users,  load average: 0.76, 0.81, 0.99

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -2,15 +2,17 @@
 #include <assert.h>
 #include <stdbool.h>
 
-static bool option_P = false;
-
 int main(int argc, char *argv[]) {
   printf("Hello, World!\n");
-  int i;
-  for (i = 0; i < argc; i++) {
+  parseOptions(argc, argv);
+  return 0;
+}
+
+void parseOptions(int argc, char *argv[]) {
+  // skip the process name (argv[0])
+  for (int i = 1; i < argc; ++i) {
     assert(argv[i]); // always true
     printf("argv[%d] = %s\n", i, argv[i]);
   }
   assert(!argv[argc]); // always true
-  return 0;
 }
