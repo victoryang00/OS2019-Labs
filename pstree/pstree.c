838baab1b10bb22c5017a8ceba78dd150cc2506a
tracer-nju <tracer@nju.edu.cn>
2019-02-26 16:11:02 +0800

    Static hostname: doowzs-eve          Icon name: computer-desktop            Chassis: desktop         Machine ID: a024f767cdeb40369ab1831e42d9d10c            Boot ID: 346c7d302d0e472c9befe7d2dfa2face   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.4.164chromium       Architecture: x86-64  16:11:02 up 1 day,  1:52,  5 users,  load average: 0.43, 0.77, 0.93

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -3,14 +3,17 @@
 #include <stdbool.h>
 
 int parseOptions(int, char*[]);
+void printPSTree();
 
 int main(int argc, char *argv[]) {
   int NR_errop = 0;
   if ((NR_errop = parseOptions(argc, argv)) != 0) {
-    fprintf(stderr, "Invalid option \"%s\". Aborted.", argv[NR_errop]);
+    // option parse failed
+    fprintf(stderr, "Invalid option \"%s\". Aborted.\n", argv[NR_errop]);
     return -1;
   } else {
-    // parse OK
+    // option parse OK
+    printPSTree();
   }
   return 0;
 }
@@ -21,7 +24,6 @@ int parseOptions(int argc, char *argv[]) {
     assert(argv[i]); // always true
     printf("argv[%d] = %s\n", i, argv[i]);
   }
-  return 1;
   assert(!argv[argc]); // always true
   return 0;
 }
