d9fbdc8af02e8d83d6eb98d7605cfea017a4b62c
tracer-nju <tracer@nju.edu.cn>
2019-02-26 16:20:12 +0800

    Static hostname: doowzs-eve          Icon name: computer-desktop            Chassis: desktop         Machine ID: a024f767cdeb40369ab1831e42d9d10c            Boot ID: 346c7d302d0e472c9befe7d2dfa2face   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.4.164chromium       Architecture: x86-64  16:20:12 up 1 day,  2:01,  5 users,  load average: 0.51, 0.76, 0.86

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -2,24 +2,31 @@
 #include <assert.h>
 #include <stdbool.h>
 
+/* define option struct (class) */
 struct option {
   const char* name;
   const char* full_name;
   bool* target;
 };
 
+/* 3 functionality option of the program */
 static bool OP_P, OP_N, OP_V;
 
+/* ICS-PA style options array */
 const struct option options[] = { 
   { "-p", "--show-pids",    &OP_P }, 
   { "-n", "--numeric-sort", &OP_N },
   { "-V", "--version",      &OP_V }
 };
+#define NR_OPTIONS sizeof(options) / sizeof(option)
 
+/* definition of functions */
 int parseOptions(int, char*[]);
 void printPSTree();
 
+/* main entry of the program */
 int main(int argc, char *argv[]) {
+  printf("sizeof options is %d\n", NR_OPTIONS);
   int ErrArgc = 0;
   if ((ErrArgc = parseOptions(argc, argv)) != 0) {
     // option parse failed
