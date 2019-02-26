bbc1313c338b6a9f94ffc5a2da595c761d531823
tracer-nju <tracer@nju.edu.cn>
2019-02-26 16:07:19 +0800

    Static hostname: doowzs-eve          Icon name: computer-desktop            Chassis: desktop         Machine ID: a024f767cdeb40369ab1831e42d9d10c            Boot ID: 346c7d302d0e472c9befe7d2dfa2face   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.4.164chromium       Architecture: x86-64  16:07:19 up 1 day,  1:48,  5 users,  load average: 0.87, 0.92, 1.00

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -6,15 +6,22 @@ void parseOptions(int, char*[]);
 
 int main(int argc, char *argv[]) {
   printf("Hello, World!\n");
-  parseOptions(argc, argv);
+  int NR_errop = parseOptions(argc, argv);
+  if (!NR_errop) {
+    sprintf(stderr, "Invalid option %s. Aborted.", argv[NR_errop]);
+    return -1;
+  } else {
+    // parse OK
+  }
   return 0;
 }
 
-void parseOptions(int argc, char *argv[]) {
+int parseOptions(int argc, char *argv[]) {
   // skip the process name (argv[0])
   for (int i = 1; i < argc; ++i) {
     assert(argv[i]); // always true
     printf("argv[%d] = %s\n", i, argv[i]);
   }
   assert(!argv[argc]); // always true
+  return 0;
 }
