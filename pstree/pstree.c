2f42fb45d8a380ef1635afd79fcf2df5f5621580
tracer-nju <tracer@nju.edu.cn>
2019-02-26 23:56:10 +0800

    Static hostname: doowzs-eve          Icon name: computer-desktop            Chassis: desktop         Machine ID: a024f767cdeb40369ab1831e42d9d10c            Boot ID: 346c7d302d0e472c9befe7d2dfa2face   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.4.164chromium       Architecture: x86-64  23:56:10 up 1 day,  9:37,  6 users,  load average: 0.01, 0.12, 0.16

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -188,5 +188,6 @@ void printParentProcesses(struct process* proc) {
   /* Print the vertical lines of parent processes */
   if (!proc->parent) return;
   printParentProcesses(proc->parent);
-  printf("%*s | ", (int) strlen(proc->name), "");
+  printf("%*s", (int) strlen(proc->name), "");
+  printf(" | ");
 }
