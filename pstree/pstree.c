c14fe74002bfd4e423f30f44af7db433e4f616c5
tracer-nju <tracer@nju.edu.cn>
2019-02-26 16:23:31 +0800

    Static hostname: doowzs-eve          Icon name: computer-desktop            Chassis: desktop         Machine ID: a024f767cdeb40369ab1831e42d9d10c            Boot ID: 346c7d302d0e472c9befe7d2dfa2face   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.4.164chromium       Architecture: x86-64  16:23:31 up 1 day,  2:04,  5 users,  load average: 2.24, 1.46, 1.11

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -18,7 +18,7 @@ const struct option options[] = {
   { "-n", "--numeric-sort", &OP_N },
   { "-V", "--version",      &OP_V }
 };
-#define NR_OPTIONS sizeof(options) / sizeof(struct option)
+const size_t NR_OPTIONS = sizeof(options) / sizeof(struct option)
 
 /* definition of functions */
 int parseOptions(int, char*[]);
