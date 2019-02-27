b07f87c22d28bf6bc0a03ce9c52f1e965a2cd672
tracer-nju <tracer@nju.edu.cn>
2019-02-27 15:31:33 +0800

    Static hostname: doowzs-eve          Icon name: computer-desktop            Chassis: desktop         Machine ID: a024f767cdeb40369ab1831e42d9d10c            Boot ID: 346c7d302d0e472c9befe7d2dfa2face   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.4.164chromium       Architecture: x86-64  15:31:33 up 2 days,  1:12,  7 users,  load average: 1.14, 1.03, 0.97

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -153,7 +153,7 @@ void readProcess(char* pidStr, char* taskPidStr) {
     if (taskPidStr) {
       proc->ppid = (pid_t) strtol(pidStr, NULL, 10);
       struct process* parent = findProcess(proc->ppid, NULL);
-      sprintf(proc->name, "{%.16s}", parent->name);
+      sprintf(proc->name, "{%.16s}", parent ? parent->name : "FUCK");
     }
     if (OP_SHOWPID) printProcessPID(proc); 
     proc->parent = proc->child = proc->next = NULL;
