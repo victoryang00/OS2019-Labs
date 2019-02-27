04a6b0dba0bc0045bc42ea60f28abc19d7f282ad
tracer-nju <tracer@nju.edu.cn>
2019-02-27 11:32:53 +0800

    Static hostname: doowzs-eve          Icon name: computer-desktop            Chassis: desktop         Machine ID: a024f767cdeb40369ab1831e42d9d10c            Boot ID: 346c7d302d0e472c9befe7d2dfa2face   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.4.164chromium       Architecture: x86-64  11:32:53 up 1 day, 21:14,  4 users,  load average: 0.43, 0.34, 0.38

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -192,7 +192,7 @@ void addProcess(struct process* proc) {
 
 void printProcess(struct process* proc) {
   printf("%s%s%s%s", 
-      (proc == &rootProcess ? "" : (proc == proc->parent->child ? "---" : (proc->next ? " +-" : " \\-"))), 
+      (proc == &rootProcess ? "" : (proc == proc->parent->child ? (proc->next ? "-+-" : "---") : (proc->next ? " +-" : " \\-"))), 
       proc->name, OP_SHOWPID ? proc->pidStr : "", 
       proc->child ? "" : "\n");
   
