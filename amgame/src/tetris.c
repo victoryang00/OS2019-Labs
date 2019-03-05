d781a35bff881d505085c1370bbc6762b85a1d26
tracer-nju <tracer@nju.edu.cn>
2019-03-05 23:23:13 +0800

    Static hostname: doowzs-eve          Icon name: computer-desktop            Chassis: desktop         Machine ID: a024f767cdeb40369ab1831e42d9d10c            Boot ID: 38244ed2f0b64e4fbfb36360ed97ed60   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.4.164chromium       Architecture: x86-64  23:23:13 up  9:20,  4 users,  load average: 3.50, 4.35, 2.74

--- amgame/src/tetris.c
+++ amgame/src/tetris.c
@@ -246,8 +246,8 @@ void drawGameSection(struct Tetromino T) {
 
 void drawRightSection() {
   struct Point p;
-  clearScreen(state.tetrominosBias.x - 3 * state.blockSide, state.tetrominosBias.y - 3 * state.blockSide, (3 + 6 * NR_TETROMINOS) * state.blockSide, (3 + 6 * NR_TETROMINOS) * state.blockSide);
-  drawString("NEXT", state.tetrominosBias.x - 2 * state.blockSide, state.tetrominosBias.y - 2 * state.blockSide);
+  clearScreen(state.tetrominosBias.x - 3 * state.blockSide, state.tetrominosBias.y - 3 * state.blockSide, (3 + 6 * NR_TETROMINOS) * state.blockSide, (6 + 6 * NR_TETROMINOS) * state.blockSide);
+  drawString("N E X T", state.tetrominosBias.x - state.blockSide, state.tetrominosBias.y - 2 * state.blockSide);
   for (int i = 0; i < NR_TETROMINOS; ++i) {
     int type = state.nextTypes[i];
     for (int j = 0; j < 4; ++j) {
@@ -256,4 +256,5 @@ void drawRightSection() {
       drawSquare(p, state.blockSide, tetrominoTypes[type].color);
     }
   }
+  drawString("S C O R E", state.tetrominosBias.x - state.blockSide, state.tetrominosBias.y + 6 * NR_TETROMINOS * state.blockSide);
 }
