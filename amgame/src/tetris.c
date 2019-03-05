a9233df90a581d1132cc944fce6b5b2f01090253
tracer-nju <tracer@nju.edu.cn>
2019-03-05 22:23:12 +0800

    Static hostname: doowzs-eve          Icon name: computer-desktop            Chassis: desktop         Machine ID: a024f767cdeb40369ab1831e42d9d10c            Boot ID: 38244ed2f0b64e4fbfb36360ed97ed60   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.4.164chromium       Architecture: x86-64  22:23:12 up  8:20,  7 users,  load average: 0.95, 0.71, 0.55

--- amgame/src/tetris.c
+++ amgame/src/tetris.c
@@ -243,7 +243,7 @@ void drawGameSection(struct Tetromino T) {
 
 void drawRightSection() {
   struct Point p;
-  clearScreen(state.tetrominosBias.x - 3 * state.blockSide, state.tetrominosBias.y - 3 * state.blockSide, 5 * state.blockSide, 5 * state.blockSide);
+  clearScreen(state.tetrominosBias.x - 3 * state.blockSide, state.tetrominosBias.y - 3 * state.blockSide, (3 + 5 * NR_TETROMINOS) state.blockSide, (3 + 5 * NR_TETROMINOS) * state.blockSide);
   for (int i = 0; i < NR_TETROMINOS; ++i) {
     int type = state.nextTypes[i];
     for (int j = 0; j < 4; ++j) {
