2c398207315c01f166eab691ac0b81edc579e8b0
tracer-nju <tracer@nju.edu.cn>
2019-03-05 10:18:37 +0800

    Static hostname: doowzs-eve          Icon name: computer-desktop            Chassis: desktop         Machine ID: a024f767cdeb40369ab1831e42d9d10c            Boot ID: 63334078384049a1b0f85e69a5d49150   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.4.164chromium       Architecture: x86-64  10:18:37 up 5 days,  2:17,  4 users,  load average: 1.19, 1.62, 1.36

--- amgame/src/tetris.c
+++ amgame/src/tetris.c
@@ -154,7 +154,7 @@ int checkPoint(struct Point p) {
   else {
     if (p.y < 0) return -1; // above screen
     if (p.y >= SCREEN_H) return 0;
-    return screen[p.y][p.x] ? 0 : 1;
+    return screen[p.y][p.x] == 0 ? 0 : 1;
   }
 }
 
