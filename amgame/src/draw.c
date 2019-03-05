bc41d4fdbd0945a46bbcd074db21f4a279f6cd9e
tracer-nju <tracer@nju.edu.cn>
2019-03-05 23:13:12 +0800

    Static hostname: doowzs-eve          Icon name: computer-desktop            Chassis: desktop         Machine ID: a024f767cdeb40369ab1831e42d9d10c            Boot ID: 38244ed2f0b64e4fbfb36360ed97ed60   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.4.164chromium       Architecture: x86-64  23:13:12 up  9:10,  4 users,  load average: 0.50, 0.54, 0.71

---
+++ amgame/src/draw.c
@@ -0,0 +1,38 @@
+#include "game.h"
+
+extern struct State state;
+extern char font8x8_basic[128][8];
+
+char *itoa(int n)  {  
+  static char s[64];
+  int i = sizeof(s) - 1;
+  do {
+    s[--i] = n % 10 + '0';  
+    n /= 10;
+  } while(n > 0);  
+  return &s[i];
+}  
+
+static inline void drawCharacter(char ch, int x, int y, uint32_t color) {
+  int i, j;
+  char *p = font8x8_basic[(int)ch];
+  uint32_t black = 0x000000, white = 0xffffff;
+  for (i = 0; i < 8; i ++) {
+    for (j = 0; j < 8; j ++) { 
+      if (x + j < state.width && y + i < state.height) {
+        draw_rect((p[i] >> j) & 1 ? &white : &black, x + j, y + i, 1, 1);
+      }
+    }
+  }
+}
+
+static inline void drawString(const char *str, int x, int y) {
+  while (*str) {
+    draw_character(*str ++, x, y);
+    if (x + 8 >= W) {
+      y += 8; x = 0;
+    } else {
+      x += 8;
+    }
+  }
+}
