26bc465a59803d843b327864b24611dcb1d82286
tracer-nju <tracer@nju.edu.cn>
2019-03-05 16:14:46 +0800

    Static hostname: doowzs-eve          Icon name: computer-desktop            Chassis: desktop         Machine ID: a024f767cdeb40369ab1831e42d9d10c            Boot ID: 38244ed2f0b64e4fbfb36360ed97ed60   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.4.164chromium       Architecture: x86-64  16:14:46 up  2:12,  4 users,  load average: 0.77, 1.38, 1.29

--- amgame/src/tetris.c
+++ amgame/src/tetris.c
@@ -186,11 +186,10 @@ bool checkRow(int row) {
 void deleteRows(int row, int combo) {
   assert(row >= 0 && combo > 0 && row + combo < SCREEN_H);
   for (int i = row; i >= 0; --i) {
-    for (int j = 0; j < SCREEN_W; ++j) {
-      screen[i + combo][j] = screen[i][j];
-    }
+    memcpy(&screen[i + combo][0], &screen[i][0], SCREEN_W * sizeof(struct Tetromino));
   }
   memset(screen, 0, combo * SCREEN_W * sizeof(struct Tetromino));
+  for (int i = 0; i < SCREEN_W; ++i) Log("(%d, %d)->%d", i, 2, screen[2][i]);
 }
 
 void drawBlock(struct Point pos, uint32_t color) {
