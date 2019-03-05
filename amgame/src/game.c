d52602c710cd017e0321afeb5dd7b0a1c1c9f3f3
tracer-nju <tracer@nju.edu.cn>
2019-03-05 22:15:11 +0800

    Static hostname: doowzs-eve          Icon name: computer-desktop            Chassis: desktop         Machine ID: a024f767cdeb40369ab1831e42d9d10c            Boot ID: 38244ed2f0b64e4fbfb36360ed97ed60   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.4.164chromium       Architecture: x86-64  22:15:11 up  8:12,  7 users,  load average: 0.07, 0.30, 0.43

--- amgame/src/game.c
+++ amgame/src/game.c
@@ -54,9 +54,9 @@ void gameInit() {
 }
 
 void clearScreen(int x, int y, int w, int h) {
-  Log("START, (%d, %d), size=%d*%d", x, y, w, h);
   uint32_t pixels[w][h]; // CAUTION!
   memset(pixels, 0x00, sizeof(pixels));
+  Log("START, (%d, %d), size=%d*%d", x, y, w, h);
   draw_rect((uint32_t*) pixels, x, y, w, h);
   Log("END");
 }
