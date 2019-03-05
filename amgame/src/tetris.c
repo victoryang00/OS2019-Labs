85f68713a0da76b6e9d8f11de94317c17268f666
tracer-nju <tracer@nju.edu.cn>
2019-03-05 10:19:11 +0800

    Static hostname: doowzs-eve          Icon name: computer-desktop            Chassis: desktop         Machine ID: a024f767cdeb40369ab1831e42d9d10c            Boot ID: 63334078384049a1b0f85e69a5d49150   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.4.164chromium       Architecture: x86-64  10:19:11 up 5 days,  2:17,  4 users,  load average: 2.11, 1.77, 1.42

--- amgame/src/tetris.c
+++ amgame/src/tetris.c
@@ -101,7 +101,7 @@ struct Tetromino fallTetromino(struct Tetromino originT, bool force) {
   } else {
     struct Tetromino nextT = T;
     nextT.pos.y++;
-    T = checkTetromino(nextT) ? nextT : T;
+    T = checkTetromino(nextT) != 0 ? nextT : T;
   }
   if (force || memcmp(&T, &originT, sizeof(struct Tetromino)) == 0) {
     if (checkTetromino(T) == -1) {
