c175775c70a07c9a7c843f0f7229e5296c4b8d04
tracer-nju <tracer@nju.edu.cn>
2019-03-12 21:48:33 +0800

    Static hostname: doowzs-eve          Icon name: computer-desktop            Chassis: desktop         Machine ID: 3a368fbc05d24fddaf409f5bd77a6b79            Boot ID: bfcc6f7ffc184da194d34a357fa7a411   Operating System: Ubuntu 18.10             Kernel: Linux 4.4.176chromium-gac02120c       Architecture: x86-64  21:48:33 up 1 day, 11:26,  5 users,  load average: 0.87, 0.59, 0.59

--- libco/co.c
+++ libco/co.c
@@ -60,6 +60,8 @@ struct co* co_start(const char* name, func_t func, void* arg) {
     /* continue from co_wait */
     Log("FINISHEDDDD");
     longjmp(wait_buf, 1);
+  } else {
+    stackOFF(stack_backup);
   }
   /* continue from co_yield */
   return current;
@@ -69,8 +71,6 @@ void co_yield() {
   Log("co_yield called by CO [%s]!", current->name);
   if (!setjmp(current->buf)) {
     if (current->state == ST_I) {
-      Log("INIT!");
-      stackOFF(stack_backup);
       current->state = ST_S;
       longjmp(start_buf, 1);
       /* go back to co_start */
