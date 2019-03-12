8930fc71b32c67d23b90cfa857aebe7dc1eac8a1
tracer-nju <tracer@nju.edu.cn>
2019-03-12 21:37:14 +0800

    Static hostname: doowzs-eve          Icon name: computer-desktop            Chassis: desktop         Machine ID: 3a368fbc05d24fddaf409f5bd77a6b79            Boot ID: bfcc6f7ffc184da194d34a357fa7a411   Operating System: Ubuntu 18.10             Kernel: Linux 4.4.176chromium-gac02120c       Architecture: x86-64  21:37:14 up 1 day, 11:15,  5 users,  load average: 0.33, 0.38, 0.55

--- libco/co.h
+++ libco/co.h
@@ -54,7 +54,6 @@ inline void stackON(struct co* cp, void* sp_ptr) {
   asm volatile("mov " SP ", %0" : "=g"(sp_ptr) :);
   Log("BP=>%p", bp_ptr);
   Log("SP=>%p", sp_ptr);
-  size_t sz_delta = sp_ptr - bp_ptr;
   asm volatile("mov %0, " SP : : "g"(cp->stack_ptr));
   Log("STACK ON!");
 }
