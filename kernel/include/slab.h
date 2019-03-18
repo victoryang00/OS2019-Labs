b6174b5890dc37a2fb3d1e14332884e0be331ca7
tracer-nju <tracer@nju.edu.cn>
2019-03-19 00:11:06 +0800

    Static hostname: doowzs-eve          Icon name: computer-desktop            Chassis: desktop         Machine ID: 3a368fbc05d24fddaf409f5bd77a6b79            Boot ID: f8f75d0b35f643f49a6b46fab930f390   Operating System: Ubuntu 18.10             Kernel: Linux 4.4.176chromium-gac02120c       Architecture: x86-64  00:11:06 up  9:13,  3 users,  load average: 0.16, 0.14, 0.19

--- kernel/include/slab.h
+++ kernel/include/slab.h
@@ -1,10 +1,12 @@
 #ifndef __SLAB_H__
 #define __SLAB_H__
 
-#define Byte sizeof(uint8_t)
-#define KB 1024 * Byte
-#define SZ_PAGE 4 * KB
-#define NR_ITEMS_MAX 64
+#define Byte           sizeof(uint8_t)
+#define KB             1024 * Byte
+#define SZ_PAGE        4 * KB
+#define SZ_SMALL_OBJ   SZ_PAGE / 8
+#define NR_CACHE_PAGES 2
+#define NR_LARGE_ITEMS 4
 
 struct _slab_chain;
 struct _slab_chain_head;
@@ -25,7 +27,8 @@ struct _slab_chain_head {
 
 struct _slab_cache {
   size_t item_size;
-  uint32_t items_per_page;
+  int items_per_chain;
+  int pages_per_chain;
   struct _slab_chain_head slab_free;
   struct _slab_chain_head slab_full;
 };
@@ -34,14 +37,18 @@ struct _slab_cache_head {
   int nr_pages;
   void *page_memory;
   bool *page_indicators;
+  struct _slab_cache* cache_memory;
   struct _slab_cache* prev;
   struct _slab_cache* next;
 };
 extern struct _slab_cache_head *slab_master;
 
+
+
 void slab_init(void *, void *);
-void *get_free_page();
-void free_used_page(void *);
+struct _slab_cache *slab_cache_create(size_t);
+void slab_cache_grow(struct _slab_cache *);
+
 
 static inline void *page_translate(int n) {
   return slab_master->page_memory + n * SZ_PAGE;
@@ -49,6 +56,32 @@ static inline void *page_translate(int n) {
 
 static inline bool *page_indicator(int n) {
   return slab_master->page_indicators + n;
+}
+
+static inline void *get_free_page(int n) {
+  bool success;
+  for (int i = 0; i < slab_master->nr_pages - n; ++i) {
+    success = true;
+    for (int j = 0; j < n; ++j) {
+      if (likely(*page_indicator(i + j))) {
+        success = false;
+        break;
+      }
+    }
+    if (success) {
+      for (int j = 0; j < n; ++j) {
+        *page_indicator(i + j) = true;
+      }
+      return page_translate(i);
+    }
+  }
+  Log("NO FREE PAGES!");
+  assert(0);
+}
+
+static inline void free_used_page(void *pg_base) {
+  int nr_pg = (pg_base - slab_master->page_memory) / SZ_PAGE;
+  *page_indicator(nr_pg) = false;
 }
 
 #endif
--- kernel/src/slab.c
+++ kernel/src/slab.c
@@ -6,23 +6,39 @@ struct _slab_cache_head *slab_master = &_master;
 
 void slab_init(void *heap_start, void *heap_end) {
   assert(heap_end > heap_start);
-  slab_master->nr_pages = (heap_end - heap_start) / (SZ_PAGE + sizeof(bool));
+  slab_master->nr_pages = (heap_end - heap_start) / (SZ_PAGE + sizeof(bool)) - NR_CACHE_PAGES;
+  assert(slab_master->nr_pages > 0);
   slab_master->page_memory = heap_start;
-  slab_master->page_indicators = heap_start + slab_master->nr_pages * SZ_PAGE;
+  slab_master->page_indicators = (bool *) (heap_start + (slab_master->nr_pages + NR_CACHE_PAGES) * SZ_PAGE);
+  slab_master->cache_memory = (struct _slab_cache *) (heap_start + slab_master->nr_pages * SZ_PAGE);
   memset(slab_master->page_indicators, 0, slab_master->nr_pages * sizeof(bool));
 }
 
-void* get_free_page() {
-  for (int i = 0; i < slab_master->nr_pages; ++i) {
-    if (*page_indicator(i)) {
-      return page_translate(i);
-    }
+struct _slab_cache *slab_cache_create(size_t size) {
+  struct _slab_cache *p_cache = slab_master->cache_memory++;
+  assert((void *) slab_master->cache_memory < (void *) slab_master->page_indicators);
+  p_cache->size = size;
+  if (size <= SZ_SMALL_OBJ) {
+    p_cache->items_per_chain = (SZ_PAGE - sizeof(struct _slab_chain)) / size;
+  } else {
+    p_cache->items_per_chain = NR_LARGE_ITEMS;
   }
-  Log("NO FREE PAGES!");
-  assert(0);
+  // nr_pages should be ceil of the result.
+  p_cache->pages_per_chain = (p_cache->items_per_chain * size + sizeof(struct _slab_chain) - 1) / SZ_PAGE + 1;
+  return p_cache;
 }
 
-void free_used_page(void *pg_base) {
-  int nr_pg = (pg_base - slab_master->page_memory) / SZ_PAGE;
-  *page_indicator(nr_pg) = false;
+void slab_cache_grow(struct _slab_cache *p_cache) {
+  void *page = NULL;
+  struct _slab_chain *p_chain;
+  page = get_free_page(p_cache->pages_per_allocate);
+  p_chain = page + p_cache->pages_per_allocate * SZ_PAGE - sizeof(struct _slab_chain);
+  assert(p_chain); // NOT NULL
+  p_chain->page = page;
+  p_chain->next = p_cache;
+  p_chain->prev = p_cache->prev;
+  p_cache->prev->next = p_chain;
+  p_cache->prev = p_chain;
+  p_chain->parent = p_cache;
+  return p_chain;
 }
