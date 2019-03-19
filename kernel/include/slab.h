95ef9d729e874ff7a1cb95a1939830d5f7a200fc
tracer-nju <tracer@nju.edu.cn>
2019-03-19 10:57:01 +0800

    Static hostname: doowzs-eve          Icon name: computer-desktop            Chassis: desktop         Machine ID: 3a368fbc05d24fddaf409f5bd77a6b79            Boot ID: f8f75d0b35f643f49a6b46fab930f390   Operating System: Ubuntu 18.10             Kernel: Linux 4.4.176chromium-gac02120c       Architecture: x86-64  10:57:01 up 19:59,  3 users,  load average: 0.41, 0.36, 0.36

--- kernel/include/slab.h
+++ kernel/include/slab.h
@@ -74,8 +74,8 @@ static inline void kmem_slab_chain_remove(struct kmem_slab *head, struct kmem_sl
 }
 
 static inline void kmem_cache_move_slab_to_full(struct kmem_cache *cp, struct kmem_slab *sp) {
-  slab_chain_remove(cp->slabs_free, sp);
-  slab_chain_add(cp->slabs_full, sp);
+  kmem_slab_chain_remove(cp->slabs_free, sp);
+  kmem_slab_chain_add(cp->slabs_full, sp);
 }
 
 static inline void kmem_cache_move_slab_to_free(struct kmem_cache *cp, struct kmem_slab *sp) {
@@ -94,7 +94,7 @@ static inline void kmem_slab_add_item(struct kmem_slab *sp, struct kmem_item *it
   }
   sp->nr_items++;
   if (sp->nr_items >= sp->nr_items_max) {
-    move_slab_to_full(sp);
+    kmem_cache_move_slab_to_full(sp);
   }
 }
 
@@ -115,7 +115,7 @@ static inline void kmem_slab_remove_item(struct kmem_slab *sp, struct kmem_item 
   }
   item->next = NULL;
   if (sp->nr_items >= sp->nr_items_max) {
-    move_slab_to_free(sp);
+    kmem_cache_move_slab_to_free(sp);
   }
   sp->nr_items--;
 }
