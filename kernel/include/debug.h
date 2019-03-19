b07298ceb70a4b3a4bfc106ba997e6f5570da983
tracer-nju <tracer@nju.edu.cn>
2019-03-19 14:26:47 +0800

    Static hostname: doowzs-eve          Icon name: computer-desktop            Chassis: desktop         Machine ID: 3a368fbc05d24fddaf409f5bd77a6b79            Boot ID: f8f75d0b35f643f49a6b46fab930f390   Operating System: Ubuntu 18.10             Kernel: Linux 4.4.176chromium-gac02120c       Architecture: x86-64  14:26:47 up 23:29,  4 users,  load average: 0.39, 0.73, 0.59

--- kernel/include/debug.h
+++ kernel/include/debug.h
@@ -3,12 +3,12 @@
 
 #ifdef DEBUG
 #define Log(format, ...) \
-  printf("\33[0m\33[1;44m[I][%s,%d,%s] " format "\33[0m\n", \
+  printf("\33[0m\33[1;44m[%s,%d,%s] " format "\33[0m\n", \
     __FILE__, __LINE__, __func__, ## __VA_ARGS__)
 #define Assert(cond, format, ...) \
   do { \
     if (!(cond)) { \
-      printf("\33[0m[\33[1;31mE\33[0m]\33[1;34m[%s,%d,%s] " format "\33[0m\n", \
+      printf("\33[0m\33[1;41m[%s,%d,%s] " format "\33[0m\n", \
           __FILE__, __LINE__, __func__, ## __VA_ARGS__); \
       assert(cond); \
     } \
