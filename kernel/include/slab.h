8ce38fbb2f266da9d6553f73a895e2dad9e21436
tracer-nju <tracer@nju.edu.cn>
2019-03-19 10:52:22 +0800

    Static hostname: doowzs-eve          Icon name: computer-desktop            Chassis: desktop         Machine ID: 3a368fbc05d24fddaf409f5bd77a6b79            Boot ID: f8f75d0b35f643f49a6b46fab930f390   Operating System: Ubuntu 18.10             Kernel: Linux 4.4.176chromium-gac02120c       Architecture: x86-64  10:52:22 up 19:55,  2 users,  load average: 0.39, 0.36, 0.36

--- kernel/include/slab.h
+++ kernel/include/slab.h
@@ -8,5 +8,114 @@
 #define NR_CACHE_PAGES 2
 #define NR_LARGE_ITEMS 4
 
+struct kmem_item;
+struct kmem_slab;
+struct kmem_cache;
+
+struct kmem_item {
+  bool used;
+  struct kmem_slab *slab;
+}
+
+struct kmem_slab {
+  size_t item_size;
+  int nr_items;
+  int nr_items_max;
+  void *pg_start;
+  int nr_pages;
+  struct kmem_item *items;
+  struct kmem_cache *cache;
+}
+
+struct kmem_cache {
+  size_t item_size;
+  int nr_items_slab;
+  int nr_pages_alloc;
+  struct kmem_slab *slabs_free;
+  struct kmem_slab *slabs_full;
+}
+
+void kmem_init(void *, void *);
+struct kmem_cache* kmem_cache_create(size_t);
+void kmem_cache_grow(struct kmem_cache *cp);
+
+void *get_free_pages(int);
+void free_used_pages(void *, int);
+
+struct inline void kmem_slab_chain_add(struct kmem_slab *head, struct kmem_slab *slab) {
+  slab->next = NULL;
+  if (head) {
+    struct kmem_slab *sp = head;
+    while (sp->next) sp = sp->next;
+    sp->next = slab;
+  } else {
+    head = slab;
+  }
+}
+
+struct inline void kmem_slab_chain_remove(struct kmem_slab *head, struct kmem_slab *slab) {
+  if (head == slab) {
+    head = slab->next;
+  } else {
+    struct kmem_slab *sp = head;
+    bool success = false;
+    for ( ; sp && sp->next; sp = sp->next) {
+      if (sp->next == slab) {
+        sp->next = slab->next;
+        success = true;
+        break;
+      }
+    }
+    assert(likely(success));
+  }
+  slab->next = NULL;
+}
+
+static inline void kmem_cache_move_slab_to_full(struct kmem_cache *cp, struct kmem_slab *sp) {
+  slab_chain_remove(cp->slab_free, sp);
+  slab_chain_add(cp->slab_full, sp);
+}
+
+static inline void kmem_cache_move_slab_to_free(struct kmem_cache *cp, struct kmem_slab *sp) {
+  slab_chain_remove(cp->slab_full, sp);
+  slab_chain_add(cp->slab_free, sp);
+}
+
+static inline void kmem_slab_add_item(struct kmem_slab *sp, struct kmem_item *item) {
+  item->next = NULL;
+  if (sp->items) {
+    struct kmem_item *ip = sp->items;
+    while (ip->next) ip = ip->next;
+    ip->next = item;
+  } else {
+    sp->items = item;
+  }
+  sp->nr_items++;
+  if (sp->nr_items >= sp->nr_items_max) {
+    move_slab_to_full(sp);
+  }
+}
+
+static inline void kmem_slab_remove_item(struct kmem_slab *sp, struct kmem_item *item) {
+  if (sp->items == item) {
+    sp->items = sp->items->next;
+  } else {
+    struct kmem_item *ip = sp->items;
+    bool success = false;
+    for ( ; ip && ip->next; ip = ip->next) {
+      if (ip->next == item) {
+        ip->next= item->next;
+        success = true;
+        break;
+      }
+    }
+    assert(likely(success));
+  }
+  item->next = NULL;
+  if (sp->nr_items >= sp->nr_items_max) {
+    move_slab_to_free(sp);
+  }
+  sp->nr_items--;
+}
 
 #endif
--- kernel/src/slab.c
+++ kernel/src/slab.c
@@ -1,3 +1,79 @@
 #include <common.h>
 #include <slab.h>
 
+int nr_pages = 0;
+void *pm = NULL; // paging memory
+void *pi = NULL; // paging indicators
+void *kc = NULL; // kmem caches
+
+void kmem_init(void *heap_start, void *heap_end) {
+  assert(heap_end > heap_start);
+  nr_pages = (heap_end - heap_start) / (SZ_PAGE + sizeof(bool)) - NR_CACHE_PAGES;
+  pm = heap_start;
+  kc = heap_start + nr_pages * SZ_PAGE;
+  pi = kc + NR_CACHE_PAGES * SZ_PAGE;
+  memset(pi, 0, nr_pages * sizeof(bool));
+}
+
+struct kmem_cache* kmem_cache_create(size_t size) {
+  if (kc >= pi) return NULL;
+  struct kmem_cache* cp = (struct kmem_cache *)kc;
+  kc += sizeof(kmem_cache);
+  
+  cp->item_size = sizeof(struct kmem_item) + size;
+  if (cp->item_size <= SZ_SMALL_OBJ) {
+    cp->nr_items_slab = (SZ_PAGE - sizeof(struct kmem_slab)) / cp->item_size;
+    cp->nr_pages_alloc = 1;
+  } else {
+    cp->nr_items_slab = NR_LARGE_ITEMS;
+    cp->nr_pages_alloc = (cp->item_size * NR_LARGE_ITEMS + sizeof(struct kmem_slab) - 1) / SZ_PAGE;
+  }
+  cp->slabs_free = NULL;
+  cp->slabs_full = NULL;
+  return cp;
+}
+
+void kmem_cache_grow(struct kmem_cache *cp) {
+  void *pg_start = get_free_pages(cp->nr_pages_alloc);
+  assert(pg_start != NULL);
+  struct kmem_slab *sp = pg_start + cp->nr_pages_alloc * SZ_PAGE - sizeof(struct kmem_slab);
+  
+  sp->item_size = cp->item_size;
+  sp->nr_items = 0;
+  sp->nr_items_max = cp->nr_items_slab;
+  sp->pg_start = pg_start;
+  sp->nr_pages = cp->nr_pages_alloc;
+  sp->items = NULL;
+  sp->cache = cp;
+
+  for (int i = 0; i < sp->nr_items_max; ++i) {
+    
+  }
+}
+
+void* get_free_pages(int nr) {
+  bool success = true;
+  for (int i = 0; i < nr_pages - nr; ++i) {
+    success = true;
+    for (int j = 0; j < nr; ++j) {
+      if (likely(*(pi + i + j))) {
+        success = false;
+        break;
+      }
+    }
+    if (likely(success)) {
+      for (int j = 0; j < nr; ++j) {
+        *(pi + i + j) = true;
+      }
+      return pm + i * SZ_PAGE;
+    }
+  }
+  return NULL;
+}
+
+void free_used_pages(void *base, int nr) {
+  int b = (base - pm_start) / SZ_PAGE;
+  for (int i = 0; i < nr; ++i) {
+    *(pi + b + i) = false;
+  }
+}
