6a6272923b7aae46664425cad82a82a3fc43c249
tracer-nju <tracer@nju.edu.cn>
2019-03-19 14:41:41 +0800

    Static hostname: doowzs-eve          Icon name: computer-desktop            Chassis: desktop         Machine ID: 3a368fbc05d24fddaf409f5bd77a6b79            Boot ID: f8f75d0b35f643f49a6b46fab930f390   Operating System: Ubuntu 18.10             Kernel: Linux 4.4.176chromium-gac02120c       Architecture: x86-64  14:41:41 up 23:44,  3 users,  load average: 0.24, 0.25, 0.40

--- kernel/include/slab.h
+++ kernel/include/slab.h
@@ -1,8 +1,7 @@
 #ifndef __SLAB_H__
 #define __SLAB_H__
 
-#define Byte           sizeof(uint8_t)
-#define KB             1024 * Byte
+#define KB             1024
 #define SZ_PAGE        4 * KB
 #define SZ_SMALL_OBJ   SZ_PAGE / 8
 #define NR_CACHE_PAGES 2
