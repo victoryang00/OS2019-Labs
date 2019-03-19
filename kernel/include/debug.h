5081754964dd34595bd852b969d73e134d118b43
tracer-nju <tracer@nju.edu.cn>
2019-03-19 14:16:15 +0800

    Static hostname: doowzs-eve          Icon name: computer-desktop            Chassis: desktop         Machine ID: 3a368fbc05d24fddaf409f5bd77a6b79            Boot ID: f8f75d0b35f643f49a6b46fab930f390   Operating System: Ubuntu 18.10             Kernel: Linux 4.4.176chromium-gac02120c       Architecture: x86-64  14:16:15 up 23:18,  4 users,  load average: 0.35, 0.42, 0.43

--- kernel/include/debug.h
+++ kernel/include/debug.h
@@ -8,7 +8,8 @@
 #define Assert(cond, format, ...) \
   do { \
     if (!(cond)) { \
-      printf("\33[0m[\33[1;31mAssert\33[0m]\33[1;34m[%s,%d,%s] " format "\33[0m\n", __FILE__, __LINE__, __func__, ## __VA_ARGS__) \
+      printf("\33[0m[\33[1;31mAssert\33[0m]\33[1;34m[%s,%d,%s] " format "\33[0m\n", \
+          __FILE__, __LINE__, __func__, ## __VA_ARGS__); \
       assert(cond); \
     } \
   } while (0)
