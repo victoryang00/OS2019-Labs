3ff55dcc6a00f509216ff961a29ea7a05a792d88
tracer-nju <tracer@nju.edu.cn>
2019-03-19 14:04:23 +0800

    Static hostname: doowzs-eve          Icon name: computer-desktop            Chassis: desktop         Machine ID: 3a368fbc05d24fddaf409f5bd77a6b79            Boot ID: f8f75d0b35f643f49a6b46fab930f390   Operating System: Ubuntu 18.10             Kernel: Linux 4.4.176chromium-gac02120c       Architecture: x86-64  14:04:23 up 23:07,  4 users,  load average: 0.04, 0.12, 0.22

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -26,11 +26,11 @@ static void os_run() {
 
 static void test_kmem() {
   Log("TEST START!");
-  int *a = kalloc(sizeof(int));
+  int *a = pmm->alloc(sizeof(int));
   Log("Address of a is %p", a);
   *a = 1;
   Log("Value of a is %d", *a);
-  kfree(a);
+  pmm->free(a);
   Log("TEST PASS!");
 }
 
