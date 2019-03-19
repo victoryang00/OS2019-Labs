5ede6d043e0fdd679f9b90591f5f713ff6de79f3
tracer-nju <tracer@nju.edu.cn>
2019-03-19 15:32:58 +0800

    Static hostname: doowzs-eve          Icon name: computer-desktop            Chassis: desktop         Machine ID: 3a368fbc05d24fddaf409f5bd77a6b79            Boot ID: f8f75d0b35f643f49a6b46fab930f390   Operating System: Ubuntu 18.10             Kernel: Linux 4.4.176chromium-gac02120c       Architecture: x86-64  15:32:58 up 1 day, 35 min,  4 users,  load average: 3.27, 3.21, 2.28

--- kernel/include/debug.h
+++ kernel/include/debug.h
@@ -1,21 +1,23 @@
 #ifndef __DEBUG_H__
 #define __DEBUG_H__
 
-enum BG_COLOR {
-  black  = 40,
-  red    = 41,
-  green  = 42,
-  yellow = 43,
-  blue   = 44,
-  purple = 45,
-  cyan   = 46,
-  white  = 47
-};
+#define BG_BLACK  "\033[1;40m"
+#define BG_RED    "\033[1;41m"
+#define BG_GREEN  "\033[1;42m"
+#define BG_YELLOW "\033[1;43m"
+#define BG_BLUE   "\033[1;44m"
+#define BG_PURPLE "\033[1;45m"
+#define BG_CYAN   "\033[1;46m"
+#define BG_WHITE  "\033[1;47m"
 
 #ifdef DEBUG
 #define Log(format, ...) \
   printf("\33[0m\33[1;44m[%d][%s,%d,%s] " format "\33[0m\n", \
     _cpu(), __FILE__, __LINE__, __func__, ## __VA_ARGS__)
+#define CLog(color, format, ...) \
+  printf("\33[0m" color "[%d][%s,%d,%s] " format "\33[0m\n", \
+    _cpu(), __FILE__, __LINE__, __func__, ## __VA_ARGS__)
+
 #else
 #define Log(format, ...) ;
 #endif
--- kernel/include/lock.h
+++ kernel/include/lock.h
@@ -1,7 +1,10 @@
 #ifndef __LOCK_H__
 #define __LOCK_H__
 
+#include <debug.h>
+
 void lock(volatile int *exclusion) {
+  CLog(BG_YELLOW, "LOCK ON");
   while (__sync_lock_test_and_set(exclusion, 1)) {
     while (*exclusion) {
       ;
@@ -10,6 +13,7 @@ void lock(volatile int *exclusion) {
 }
 
 void unlock(volatile int *exclusion) {
+  CLog(BG_YELLOW, "LOCK OFF");
   __sync_synchronize();
   *exclusion = 0;
 }
