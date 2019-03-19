1f5146c40034c728cbe29567bb1b1af94c5237d2
tracer-nju <tracer@nju.edu.cn>
2019-03-19 18:19:19 +0800

    Static hostname: doowzs-eve          Icon name: computer-desktop            Chassis: desktop         Machine ID: 3a368fbc05d24fddaf409f5bd77a6b79            Boot ID: f8f75d0b35f643f49a6b46fab930f390   Operating System: Ubuntu 18.10             Kernel: Linux 4.4.176chromium-gac02120c       Architecture: x86-64  18:19:19 up 1 day,  3:21,  2 users,  load average: 0.25, 0.25, 0.27

--- kernel/src/slab.c
+++ kernel/src/slab.c
@@ -113,7 +113,7 @@ void* get_free_pages(int nr) {
     if (likely(success)) {
       for (int j = 0; j < nr; ++j) {
         *(pi + i + j) = true;
-        Log("%s", *(pi) ? "true" : "false");
+        Log("pi %p => %s", pi, *(pi) ? "true" : "false");
       }
       Log("Memory pages start at %p.", pm + i * SZ_PAGE);
       return pm + i * SZ_PAGE;
