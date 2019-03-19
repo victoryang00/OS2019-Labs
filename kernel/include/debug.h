78ec512f332b0eba1220edc68fdb261ecb7db2ce
Tianyun Zhang <doowzs@qq.com>
2019-03-19 15:35:17 +0800

    upgrade debugger to support custom colors

--- kernel/include/debug.h
+++ kernel/include/debug.h
@@ -1,6 +1,15 @@
 #ifndef __DEBUG_H__
 #define __DEBUG_H__
 
+#define FG_BLACK  "\033[1;30m"
+#define FG_RED    "\033[1;31m"
+#define FG_GREEN  "\033[1;32m"
+#define FG_YELLOW "\033[1;33m"
+#define FG_BLUE   "\033[1;34m"
+#define FG_PURPLE "\033[1;35m"
+#define FG_CYAN   "\033[1;36m"
+#define FG_WHITE  "\033[1;37m"
+
 #define BG_BLACK  "\033[1;40m"
 #define BG_RED    "\033[1;41m"
 #define BG_GREEN  "\033[1;42m"
