8c7eeec487fe6d499f8da34f54f8b5cb2c0c282f
tracer-nju <tracer@nju.edu.cn>
2019-03-31 19:08:20 +0800

    Static hostname: doowzs-eve          Icon name: computer-desktop            Chassis: desktop         Machine ID: 3a368fbc05d24fddaf409f5bd77a6b79            Boot ID: 804760b320194b3085b8c79b30a31539   Operating System: Ubuntu 18.10             Kernel: Linux 4.4.176chromium-gac02120c       Architecture: x86-64  19:08:20 up 28 min,  2 users,  load average: 5.21, 3.26, 1.70

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -13,29 +13,8 @@ static void hello() {
   _putc("012345678"[_cpu()]); _putc('\n');
 }
 
-static void yls_test(){
-  void *space[100];
-  int i;
-  for(i=0;i<100;++i){
-    space[i]=pmm->alloc(rand()%((1<<10)-1));
-  }
-  CLog(BG_GREEN, "yls-OK(0) on cpu %d", _cpu());
-  for(i=0;i<1000;++i){
-    int temp=rand()%10;
-    pmm->free(space[temp]);
-    space[temp]=pmm->alloc(rand()&((1<<10)-1));
-  }
-  CLog(BG_GREEN, "yls-OK(1) on cpu %d", _cpu());
-  for(i=0;i<100;++i){
-    pmm->free(space[i]);
-  }
-  CLog(BG_GREEN, "yls-OK(2) on cpu %d", _cpu());
-  printf("Success on cpu %d", _cpu());
-}
-
 static void os_run() {
   hello();
-  yls_test();
   _intr_write(1);
   while (1) {
     _yield();
