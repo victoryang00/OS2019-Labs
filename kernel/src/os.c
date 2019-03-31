3a61e63ab80c2688680c207217cdbed963dba2cf
tracer-nju <tracer@nju.edu.cn>
2019-03-31 19:03:40 +0800

    Static hostname: doowzs-eve          Icon name: computer-desktop            Chassis: desktop         Machine ID: 3a368fbc05d24fddaf409f5bd77a6b79            Boot ID: 804760b320194b3085b8c79b30a31539   Operating System: Ubuntu 18.10             Kernel: Linux 4.4.176chromium-gac02120c       Architecture: x86-64  19:03:40 up 23 min,  2 users,  load average: 1.00, 0.89, 0.67

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -18,47 +18,6 @@ static bool B[100] = {true, false, true, false, true, false};
 static char C[100] = "AKJDjsfajfdioaoxhKJDHSJxckjzbKAJDHSFJHJKEWHJLHDSJKHFKJHSKJDHJEHWUHYROEHD";
 static double D[2000] = {0.121343287482374, 28343294, 329748238, 032103212, 820.121, 219382914.4324};
 
-static void kmem_test() {
-  int *a = pmm->alloc(sizeof(int) * 100);
-  for (int i = 0; i < 100; ++i) {
-    a[i] = A[i];
-  }
-  Assert(memcmp(a, A, sizeof(a)) == 0, "A1 CHECK FAILED");
-  bool *b = pmm->alloc(sizeof(bool) * 100);
-  for (int i = 0; i < 100; ++i) {
-    b[i] = B[i];
-  }
-  Assert(memcmp(a, A, sizeof(a)) == 0, "A2 CHECK FAILED");
-  char *c = pmm->alloc(sizeof(char) * 100);
-  for (int i = 0; i < 100; ++i) {
-    c[i] = C[i];
-  }
-  Assert(memcmp(c, C, sizeof(c)) == 0, "C1 CHECK FAILED");
-  pmm->free(c);
-  double *d = pmm->alloc(sizeof(double) * 2000);
-  for (int i = 0; i < 2000; ++i) {
-    d[i] = D[i];
-  }
-  Assert(memcmp(d, D, sizeof(d)) == 0, "D1 CHECK FAILED");
-  Assert(memcmp(b, B, sizeof(b)) == 0, "B1 CHECK FAILED");
-  pmm->free(b);
-  int **e = pmm->alloc(sizeof(int *) * 10);
-  for (int i = 0; i < 10; ++i) {
-    e[i] = pmm->alloc(sizeof(int) * 100);
-  }
-  Assert(memcmp(d, D, sizeof(d)) == 0, "D2 CHECK FAILED");
-  pmm->free(d);
-  for (int i = 0; i < 10; ++i) pmm->free(e[i]);
-  Assert(memcmp(a, A, sizeof(a)) == 0, "A3 CHECK FAILED");
-  pmm->free(a);
-  pmm->free(e);
-
-  CLog(BG_PURPLE, "============================================================");
-  CLog(BG_PURPLE, "CONGRATULATIONS. ALL STUPID TESTS HAVE BEEN PASSED on CPU %d!", _cpu());
-  CLog(BG_PURPLE, "============================================================");
-  printf("CONGRATULATIONS. ALL STUPID TESTS HAVE BEEN PASSED ON CPU %d!", _cpu());
-}
-
 static void yls_test(){
   void *space[100];
   int i;
@@ -80,8 +39,7 @@ static void yls_test(){
 
 static void os_run() {
   hello();
-  //kmem_test();
-  //yls_test();
+  yls_test();
   _intr_write(1);
   while (1) {
     _yield();
