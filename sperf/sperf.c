aec9cef6900ebf7498692ad307d5dc5c1d82163d
tracer-nju <tracer@nju.edu.cn>
2019-04-01 10:40:07 +0800

    Static hostname: doowzs-eve          Icon name: computer-desktop            Chassis: desktop         Machine ID: 3a368fbc05d24fddaf409f5bd77a6b79            Boot ID: 804760b320194b3085b8c79b30a31539   Operating System: Ubuntu 18.10             Kernel: Linux 4.4.176chromium-gac02120c       Architecture: x86-64  10:40:07 up 15:59,  3 users,  load average: 0.30, 1.00, 1.37

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -1,3 +1,37 @@
-int main(int argc, char *argv[]) {
+#include <unistd.h>
+#include "debug.h"
+
+void sperf(char *);
+
+int main(int argc, char *argv[], char *envp[]) {
+  sperf(argc, argv, envp);
   return 0;
+}
+
+void sperf(int argc, char *argv[], char *envp[]) {
+  int cpid = 0;
+  int pipefd[2] = {};
+  char buf[1024] = "";
+
+  Assert(pipe(pipefd) != -1, "Pipe failed.");
+  cpid = fork();
+  Assert(cpid != -1, "Fork failed.")
+
+  if (cpid == 0) {
+    /* child process */
+    close(pipefd[0]);
+    dup2(pipefd[1], 1); // stdout
+    dup2(pipefd[1], 2); // stderr
+    execve(argv[0], argv + 1, envp);
+    Assert(false, "Should not return from execve!");
+  } else {
+    /* parent process */
+    close(pipefd[1]);
+    while (read(pipefd[0], &buf, 1) > 0) {
+      Log("%s\n", buf);
+      // TODO
+    }
+    close(pipefd[0]);
+    wait(NULL);
+  }
 }
