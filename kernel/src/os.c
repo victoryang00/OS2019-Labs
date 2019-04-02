185a98cbb8f993c6a9bb76c0a89924db35e67483
tracer-nju <tracer@nju.edu.cn>
2019-04-02 22:48:32 +0800

    Static hostname: doowzs-eve          Icon name: computer-desktop            Chassis: desktop         Machine ID: 3a368fbc05d24fddaf409f5bd77a6b79            Boot ID: 804760b320194b3085b8c79b30a31539   Operating System: Ubuntu 18.10             Kernel: Linux 4.4.176chromium-gac02120c       Architecture: x86-64  22:48:32 up 2 days,  4:08,  5 users,  load average: 0.24, 0.38, 0.60

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -17,8 +17,8 @@ static void yls_test(){
   static void *space[999];
   int i;
   for(i=0;i<200;++i){
-    space[i]=pmm->alloc(rand()%((1<<10)-1));
     printf("%d - yls0 %d addr=%d\n, heap=[%p, %p)\n", _cpu(), i, space + i, _heap.start, _heap.end);
+    space[i]=pmm->alloc(rand()%((1<<10)-1));
   }
   for(i=0;i<100;++i){
     printf("%d - yls2 %d addr=%p, heap=[%p, %p)\n", _cpu(), i, space[i], _heap.start, _heap.end);
