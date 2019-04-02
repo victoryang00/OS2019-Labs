cac690407ef48396500268b423be11d427276c2b
tracer-nju <tracer@nju.edu.cn>
2019-04-02 15:40:09 +0800

    Static hostname: doowzs-eve          Icon name: computer-desktop            Chassis: desktop         Machine ID: 3a368fbc05d24fddaf409f5bd77a6b79            Boot ID: 804760b320194b3085b8c79b30a31539   Operating System: Ubuntu 18.10             Kernel: Linux 4.4.176chromium-gac02120c       Architecture: x86-64  15:40:09 up 1 day, 21:00,  3 users,  load average: 0.97, 0.37, 0.44

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -48,8 +48,8 @@ void child(int fd, int argc, char *argv[]) {
   memcpy(real_argv + 2, argv + 1, (argc - 1) * sizeof(char *));
 
   // not execve because we need environmental variables
-  fclose(stdout);   // stdout
-  dup2(fd, stderr); // stderr
+  fclose(1);   // stdout
+  dup2(fd, 2); // stderr
   execvp(real_argv[0], real_argv); 
   Panic("strace is not executable. (NO PATH HITS.)");
 }
