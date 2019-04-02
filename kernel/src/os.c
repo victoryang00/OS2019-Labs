d7c88157cd165483de4e26374742275716bd8ea8
tracer-nju <tracer@nju.edu.cn>
2019-04-02 23:14:33 +0800

    Static hostname: doowzs-eve          Icon name: computer-desktop            Chassis: desktop         Machine ID: 3a368fbc05d24fddaf409f5bd77a6b79            Boot ID: 804760b320194b3085b8c79b30a31539   Operating System: Ubuntu 18.10             Kernel: Linux 4.4.176chromium-gac02120c       Architecture: x86-64  23:14:33 up 2 days,  4:34,  5 users,  load average: 3.04, 3.45, 2.49

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -13,18 +13,18 @@ static void hello() {
   _putc("012345678"[_cpu()]); _putc('\n');
 }
 
+void *space[4][1000];
 static void yls_test(){
-  void *space[1000];
   int i;
   for(i=0;i<1000;++i){
     printf("%d - yls0 %d addr=%d\n, heap=[%p, %p)\n", _cpu(), i, space + i, _heap.start, _heap.end);
     size_t size = rand() % ((1 << 10) - 1) + 1;
     //Assert(size != 0, "size is 0 on round %d", i);
-    space[i]=pmm->alloc(size);
+    space[_cpu()][i]=pmm->alloc(size);
   }
   for(i=0;i<1000;++i){
     printf("%d - yls2 %d addr=%p, heap=[%p, %p)\n", _cpu(), i, space[i], _heap.start, _heap.end);
-    pmm->free(space[i]);
+    pmm->free(space[_cpu()][i]);
   }
   CLog(BG_GREEN, "OKOK on cpu %d", _cpu());
 }
