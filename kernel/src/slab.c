a89149df86f14db985de6e015d70413fa2d3ced0
tracer-nju <tracer@nju.edu.cn>
2019-04-02 23:22:34 +0800

    Static hostname: doowzs-eve          Icon name: computer-desktop            Chassis: desktop         Machine ID: 3a368fbc05d24fddaf409f5bd77a6b79            Boot ID: 804760b320194b3085b8c79b30a31539   Operating System: Ubuntu 18.10             Kernel: Linux 4.4.176chromium-gac02120c       Architecture: x86-64  23:22:34 up 2 days,  4:42,  5 users,  load average: 0.44, 2.13, 2.40

--- kernel/src/slab.c
+++ kernel/src/slab.c
@@ -97,7 +97,7 @@ void *kmem_cache_alloc(struct kmem_cache *cp) {
 void kmem_cache_free(void *ptr) {
   struct kmem_item *ip = (struct kmem_item *) (ptr - sizeof(struct kmem_item));
   struct kmem_slab *sp = ip->slab;
-  if(likely(ip->used)) {
+  if(likely(ip->used == true)) {
       CLog(BG_RED, "Access Violation: Double freeing address kmem_item %p.", ip);
   }
   ip->used = false;
