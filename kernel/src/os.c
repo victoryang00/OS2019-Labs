695468b5cef31c0c29f9bc211911b319c08c234c
tracer-nju <tracer@nju.edu.cn>
2019-04-03 14:29:16 +0800

    Static hostname: doowzs-eve          Icon name: computer-desktop            Chassis: desktop         Machine ID: 3a368fbc05d24fddaf409f5bd77a6b79            Boot ID: 8b9d9a78372f4c1a856382497bdbca12   Operating System: Ubuntu 18.10             Kernel: Linux 4.4.176chromium-gac02120c       Architecture: x86-64  14:29:16 up 19 min,  2 users,  load average: 1.00, 1.28, 0.91

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -13,21 +13,8 @@ static void hello() {
   _putc("012345678"[_cpu()]); _putc('\n');
 }
 
-static void yls_test(){
-  static void *space[1000] = {};
-  for (int i = 0; i < 1000; ++i) space[i] = pmm->alloc(rand()%((1<<10)-1));
-  for (int i = 0; i < 1000; ++i) {
-    int tmp = rand() % 10;
-    pmm->free(space[tmp]);
-    space[tmp] = pmm->alloc((1<<10)-1);
-  }
-  for (int i = 0; i < 1000; ++i) pmm->free(space[i]);
-  CLog(BG_GREEN, "OKOK on cpu %d", _cpu());
-}
-
 static void os_run() {
   hello();
-  yls_test();
   _intr_write(1);
   while (1) {
     _yield();
