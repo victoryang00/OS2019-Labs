12bfebd2852e8ef53ef6947b5031942acea12e52
tracer-nju <tracer@nju.edu.cn>
2019-04-08 11:44:08 +0800

    Static hostname: doowzs-eve          Icon name: computer-desktop            Chassis: desktop         Machine ID: 3a368fbc05d24fddaf409f5bd77a6b79            Boot ID: 51298ade1fb7475d9bf39a81f38d29ae   Operating System: Ubuntu 18.10             Kernel: Linux 4.4.176chromium-gac02120c       Architecture: x86-64  11:44:08 up 1 day, 23:11,  4 users,  load average: 0.60, 0.65, 0.61

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -122,9 +122,7 @@ void addItem(char *call_name, double call_time) {
 }
 
 void showItems() {
-  rewind(stdout);
-  ftruncate(1, 0);
-
+  printf("\033[2J\033[1;1H");
   perf_item *pp = root;
   for (; pp != NULL; pp = pp->next) {
     printf("%*s : %.5lfs (%2d%%)\n", max_name_length, pp->call_name, pp->call_time, (int) (pp->call_time / time_total * 100));
