df2d0c44c41015bd2e0b27419c0c310f65182372
tracer-nju <tracer@nju.edu.cn>
2019-04-08 14:35:49 +0800

    Static hostname: doowzs-eve          Icon name: computer-desktop            Chassis: desktop         Machine ID: 3a368fbc05d24fddaf409f5bd77a6b79            Boot ID: 96c5066da9bc486fb9bc644160ddacd7   Operating System: Ubuntu 18.10             Kernel: Linux 4.4.176chromium-gac02120c       Architecture: x86-64  14:35:49 up 32 min,  3 users,  load average: 0.35, 0.55, 0.62

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -26,7 +26,7 @@ void sperf(int argc, char *argv[]) {
 
 void child(pid_t ppid, int argc, char *argv[]) {
   char arg_output[64] = "";
-  sprintf(arg_output, "/proc/%d/fd/0", ppid);
+  sprintf(arg_output, "/proc/%d/fd/0\0", ppid);
   CLog(BG_GREEN, "write to %s", arg_output);
     
   char *real_argv[argc + 4];
