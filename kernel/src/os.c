3faca392ebf7355fee4667008631945752dbb4ae
tracer-nju <tracer@nju.edu.cn>
2019-04-08 10:37:49 +0800

    Static hostname: doowzs-eve          Icon name: computer-desktop            Chassis: desktop         Machine ID: 3a368fbc05d24fddaf409f5bd77a6b79            Boot ID: 51298ade1fb7475d9bf39a81f38d29ae   Operating System: Ubuntu 18.10             Kernel: Linux 4.4.176chromium-gac02120c       Architecture: x86-64  10:37:49 up 1 day, 22:05,  6 users,  load average: 4.33, 6.15, 4.17

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -16,7 +16,7 @@ static void hello() {
 static void *spaces[4][1005] = {};
 static void test() {
   for (int i = 0; i < 1000; ++i) {
-    spaces[_cpu()][i] = pmm->alloc(rand() % (1));
+    spaces[_cpu()][i] = pmm->alloc(rand() % (4097));
   }
   for (int i = 0; i < 1000; ++i) {
     pmm->free(spaces[_cpu()][i]);
