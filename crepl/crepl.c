f462ce10e0a3b7c37892ed7a182f35d62f1fa599
tracer-nju <tracer@nju.edu.cn>
2019-04-09 15:21:46 +0800

    Static hostname: doowzs-eve          Icon name: computer-desktop            Chassis: desktop         Machine ID: 3a368fbc05d24fddaf409f5bd77a6b79            Boot ID: 96c5066da9bc486fb9bc644160ddacd7   Operating System: Ubuntu 18.10             Kernel: Linux 4.4.176chromium-gac02120c       Architecture: x86-64  15:21:46 up 1 day,  1:18,  4 users,  load average: 0.09, 0.24, 0.26

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -1,32 +1,31 @@
 #include "crepl.h"
 
-int cnt = 0, type = 0;
-size_t len = 0;
-ssize_t nread = 0;
+int io_count = 0;
+size_t input_len = 0;
 char *input = NULL;
 char output[20] = "";
 char func_name[128] = "";
 int calc_result = 0;
 
 int main(int argc, char *argv[]) {
-  for ( ; ; ++cnt) {
-    printf(" in[%d]: ", cnt);
-    sprintf(output, "out[%d]: ", cnt);
+  for ( ; ; ++io_count) {
+    printf(" in[%d]: ", io_count);
+    sprintf(output, "out[%d]: ", io_count);
 
-    if (getline(&input, &len, stdin) < 0) break;
+    if (getline(&input, &input_len, stdin) < 0) break;
     Log("%s", input);
     printf("%s", output); 
 
     switch (precheck()) {
       case TYPE_FUNCT:
-        if (compile()) {
+        if (compile(input, input_size)) {
           printf("\33[0m" FG_GREEN "Added as function %s.\n" "\033[0m", func_name);
         } else {
           printf("\33[0m" FG_RED "Compilation error.\n" "\033[0m");
         }
         break;
       case TYPE_VALUE:
-        if (calculate()) {
+        if (calculate(input, input_size)) {
           printf("\33[0m" FG_GREEN "Result: %d.\n" "\033[0m", calc_result);
         } else {
           printf("\33[0m" FG_RED "Calculation error.\n" "\033[0m");
@@ -51,22 +50,60 @@ int precheck() {
   }
 }
 
-bool compile() {
-  // TODO: USE MKSTEMP OR TMPFILE???
-  FILE *src = tmpfile();
-  FILE *dst = tmpfile();
-  fprintf(tmp, "%s", input);
+bool compile(char *code, size_t size) {
+  /* scan the name of the function */
+  sscanf(code, " int %s ", func_name);
+
+  /* create temporaty files */
+  int fd_src = mkstemp("SRC_XXXXXX");
+  int fd_dst = mkstemp("DST_XXXXXX");
+  write(fd_src, code, size);
+
+  char file_src[128] = "";
+  char file_dst[128] = "";
+  sprintf(file_src, "/proc/self/fd/%d", fd_src);
+  sprintf(file_dst, "/proc/self/fd/%d", fd_dst);
 
   char *CC_argv[] = {
-    "gcc", "-fPIC", 
-    "-o", ""
+    "gcc", 
+    "-fPIC",   // Generate PI Code
+    "-x", "c", // Language: ANSI C
+    CC_ABI,    // ABI Type: 32, 64
+    "-w",      // Inhibit warnings
+    "-o", file_dst, file_src,
+    NULL
+  };
+
+  int pid = fork();
+  Assert(pid > 0, "Fork failed.");
+  if (pid == 0) {
+    /* fork a process to call gcc */
+    execvp(CC_argv[0], CC_argv);
+  } else {
+    /* wait for the child process */
+    wait(NULL);
   }
-  execvp(CC_argv[0], CC_argv);
-  
+
+  /* load the dynamic library */
+  dlopen(file_dst, RTLD_GLOBAL);
+
+  /* close temporaty files */
+  close(fd_src);
+  close(fd_dst);
+
   return true;
 }
 
-bool calculate() {
-  // TODO
+bool calculate(char *code, size_t size) {
+  code = func_wrapper(code, &size);
+  printf("NOT IMPLEMENTED!\n");
+  free(code);
   return true;
+}
+
+char *func_wrapper(char *code, size_t *size) {
+  char *ret = malloc(sizeof(char) * (*size + 64));
+  sprintf(ret, "int __EVAL_FUNC_%d() { %s }", io_count, code);
+  *size = strlen(ret);
+  return ret;
 }
--- crepl/crepl.h
+++ crepl/crepl.h
@@ -3,9 +3,19 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
+#include <dlfcn.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 #define DEBUG
 #include "debug.h"
+
+#if defined (__i386__)
+  #define CC_ABI "-m32"
+#elif defined (__x86_64__)
+  #define CC_ABI "-m64"
+#endif
 
 #define TYPE_INVAL 0
 #define TYPE_FUNCT 1
@@ -13,5 +23,6 @@
 
 void init();
 int precheck();
-bool compile();
-bool calculate();
+bool compile(char *, size_t);
+bool calculate(char *, size_t);
+char *func_wrapper(char *, size_t *);
