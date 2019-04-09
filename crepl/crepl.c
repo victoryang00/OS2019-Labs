e49498c59cc9fa302a2b57acbd646c0865222817
tracer-nju <tracer@nju.edu.cn>
2019-04-09 15:26:14 +0800

    Static hostname: doowzs-eve          Icon name: computer-desktop            Chassis: desktop         Machine ID: 3a368fbc05d24fddaf409f5bd77a6b79            Boot ID: 96c5066da9bc486fb9bc644160ddacd7   Operating System: Ubuntu 18.10             Kernel: Linux 4.4.176chromium-gac02120c       Architecture: x86-64  15:26:14 up 1 day,  1:23,  4 users,  load average: 0.37, 0.37, 0.30

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -52,7 +52,7 @@ int precheck() {
 
 bool compile(char *code, size_t size) {
   /* scan the name of the function */
-  sscanf(code, " int %^[)]) ", func_name);
+  sscanf(code, " int %[^)]) ", func_name);
   strcat(func_name, ")");
   CLog(BG_PURPLE, "The name of func is %s.", func_name);
 
