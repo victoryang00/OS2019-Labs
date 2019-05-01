03852173e07c4c7083d68f72e6d3ae4eb614d5e3
tracer-nju <tracer@nju.edu.cn>
2019-05-01 17:05:42 +0800

    Static hostname: doowzs-eve          Icon name: computer-convertible            Chassis: convertible         Machine ID: e1da686fb2fc4789ab3da95481c9bdeb            Boot ID: 4a7f6401ed40413d91cc05ebfd329692   Operating System: Ubuntu 19.04             Kernel: Linux 4.4.178chromium-g09559f81       Architecture: x86-64  17:05:42 up 1 day, 16:01,  7 users,  load average: 1.92, 1.68, 1.61

---
+++ kernel/include/mt-safe.h
@@ -0,0 +1,19 @@
+#ifndef __MT_SAFE_H__
+#define __MT_SAFE_H__
+
+/**
+ * MT-SAFE Wrapper: wrap the functions that is not MT-SAFE so that program
+ * will be blocked if another process is inside the function.
+ */
+
+// ----------------------------------------------------------------------------
+// printf: lock defined in src/os.c.
+
+extern struct spinlock printf_lock;
+#define printf(...) \
+    spinlock_acquire(&printf_lock); \
+    printf(__VA_ARGS__); \
+    spinlock_release(&printf_lock)
+
+
+#endif
