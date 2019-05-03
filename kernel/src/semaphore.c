ca21aebe654f2b7ed470e62c741c5bab631fb8b0
tracer-nju <tracer@nju.edu.cn>
2019-05-03 13:20:35 +0800

    Static hostname: doowzs-eve          Icon name: computer-convertible            Chassis: convertible         Machine ID: e1da686fb2fc4789ab3da95481c9bdeb            Boot ID: 97f47f57ce44483fb855fad1a9d20147   Operating System: Ubuntu 19.04             Kernel: Linux 4.4.178chromium-g09559f81       Architecture: x86-64  13:20:35 up 12:38,  4 users,  load average: 1.38, 1.35, 1.14

--- kernel/src/semaphore.c
+++ kernel/src/semaphore.c
@@ -17,9 +17,8 @@ void semaphore_init(struct semaphore *sem, const char *name, int value) {
 
 void semaphore_wait(struct semaphore *sem) {
   spinlock_acquire(&sem->lock);
-  while (sem->value < 0) {
-    // TODO:
-    // sleep(sem, &sem->lock);
+  while (sem->value <= 0) {
+    kmt_sleep(sem, &sem->lock);
   }
   --sem->value;
   spinlock_release(&sem->lock);
@@ -28,9 +27,8 @@ void semaphore_wait(struct semaphore *sem) {
 void semaphore_signal(struct semaphore *sem) {
   spinlock_acquire(&sem->lock);
   ++sem->value;
-  if (sem->value > 0) {
-    // TODO:
-    // wakeup(sem);
+  if (sem->value >= 0) {
+    kmt_wakeup(sem);
   }
   spinlock_release(&sem->lock);
 }
