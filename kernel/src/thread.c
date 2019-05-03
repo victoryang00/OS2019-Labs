37ee257eba21ac54e57ccddc82f6bbd4d067fc9f
tracer-nju <tracer@nju.edu.cn>
2019-05-03 10:03:17 +0800

    Static hostname: doowzs-eve          Icon name: computer-convertible            Chassis: convertible         Machine ID: e1da686fb2fc4789ab3da95481c9bdeb            Boot ID: 97f47f57ce44483fb855fad1a9d20147   Operating System: Ubuntu 19.04             Kernel: Linux 4.4.178chromium-g09559f81       Architecture: x86-64  10:03:17 up  9:21,  5 users,  load average: 0.36, 0.52, 0.72

--- kernel/src/thread.c
+++ kernel/src/thread.c
@@ -117,7 +117,7 @@ _Context *kmt_context_switch(_Event ev, _Context *context) {
 
 struct task *kmt_sched() {
   for (struct task *tp = &root_task; tp != NULL; tp = tp->next) {
-    //Log("%d:%s [%s]", tp->pid, tp->name, task_states_human[tp->state]);
+    Log("%d:%s [%s]", tp->pid, tp->name, task_states_human[tp->state]);
     if (tp->state == ST_E || tp->state == ST_W) { // choose a waken up task
       return tp; 
     }
