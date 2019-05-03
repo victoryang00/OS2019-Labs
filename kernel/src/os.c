fec0ed36c1f8344a3e87623c51496f3506c90f51
tracer-nju <tracer@nju.edu.cn>
2019-05-03 22:56:23 +0800

    Static hostname: doowzs-eve          Icon name: computer-convertible            Chassis: convertible         Machine ID: e1da686fb2fc4789ab3da95481c9bdeb            Boot ID: b97218f761124d6f92f3b4e6f9a88ba1   Operating System: Ubuntu 19.04             Kernel: Linux 4.4.178chromium-g09559f81       Architecture: x86-64  22:56:23 up  9:27,  5 users,  load average: 0.33, 0.56, 1.04

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -67,7 +67,9 @@ static void os_init() {
 static void os_run() {
   printf("Hello from CPU #%d\n", _cpu());
   _intr_write(1);
+  __sync_synchronize();
   Log("_intr_read() = %d", _intr_read());
+  __sync_synchronize();
   Assert(_intr_read() != 0, "Interrupt disabled");
   while (1) {
     _yield();
