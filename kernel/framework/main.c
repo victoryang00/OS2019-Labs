e7b83fc7bba2b32d13b980706177f21519f0ef31
tracer-nju <tracer@nju.edu.cn>
2019-05-03 14:06:53 +0800

    Static hostname: doowzs-eve          Icon name: computer-convertible            Chassis: convertible         Machine ID: e1da686fb2fc4789ab3da95481c9bdeb            Boot ID: b97218f761124d6f92f3b4e6f9a88ba1   Operating System: Ubuntu 19.04             Kernel: Linux 4.4.178chromium-g09559f81       Architecture: x86-64  14:06:53 up 38 min,  4 users,  load average: 0.09, 1.00, 1.68

--- kernel/framework/main.c
+++ kernel/framework/main.c
@@ -1,12 +1,41 @@
 #include <kernel.h>
 #include <klib.h>
 
+semaphore_t sem_p;
+semaphore_t sem_c;
+void customer(void *arg) {
+  while (1) {
+    kmt->sem_wait(sem_c);
+    printf(")");
+    fflush(stdout);
+    kmt->sem_signal(sem_p);
+  }
+}
+void producer(void *arg) {
+  while (1) {
+    kmt->sem_wait(sem_p);
+    printf("(");
+    fflush(stdout);
+    kmt->sem_signal(sem_c);
+  }
+}
+
 int main() {
   _ioe_init();
   _cte_init(os->trap);
 
   // call sequential init code
   os->init();
+
+  // FOR TEST PURPOSE
+  // REMOVE WHEN TEST IS PASSED
+  kmt->sem_init(sem_p, "Producer SEM", 1);
+  kmt->sem_init(sem_c, "Customer SEM", 0);
+  kmt->create(pmm->alloc(sizeof(task_t)), "Producer Task", producer, NULL);
+  kmt->create(pmm->alloc(sizeof(task_t)), "Customer Task", customer, NULL);
+
+
+
   _mpe_init(os->run); // all cores call os->run()
   return 1;
 }
