3e75d9f7319e24acee8a94ce07c7b4bd928bddc4
tracer-nju <tracer@nju.edu.cn>
2019-05-05 01:34:30 +0800

    Static hostname: doowzs-eve          Icon name: computer-convertible            Chassis: convertible         Machine ID: e1da686fb2fc4789ab3da95481c9bdeb            Boot ID: dc30a845a7a5457ab929d09156af8f15   Operating System: Ubuntu 19.04             Kernel: Linux 4.4.178chromium-g09559f81       Architecture: x86-64  01:34:31 up  7:16,  6 users,  load average: 2.51, 1.75, 1.62

--- kernel/src/thread.c
+++ kernel/src/thread.c
@@ -172,6 +172,7 @@ _Context *kmt_yield(_Event ev, _Context *context) {
   struct task *cur = get_current_task();
   struct task *next = kmt_sched(); // call scheduler
   if (!next) {
+    printf("N");
     Log("No scheduling is made.");
     if (cur) {
       cur->count = cur->count >= 1000 ? 0 : cur->count + 1;
