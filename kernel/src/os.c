76a4339be78247eed05cf80d811c38341dc3f2fc
tracer-nju <tracer@nju.edu.cn>
2019-05-04 15:18:14 +0800

    Static hostname: doowzs-eve          Icon name: computer-convertible            Chassis: convertible         Machine ID: e1da686fb2fc4789ab3da95481c9bdeb            Boot ID: b97218f761124d6f92f3b4e6f9a88ba1   Operating System: Ubuntu 19.04             Kernel: Linux 4.4.178chromium-g09559f81       Architecture: x86-64  15:18:14 up 1 day,  1:49,  5 users,  load average: 3.08, 2.14, 2.07

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -74,7 +74,7 @@ static void os_run() {
 }
 
 static _Context *os_trap(_Event ev, _Context *context) {
-  if (ev.event == _EVENT_IRQ_TIMER) {
+  if (false && ev.event == _EVENT_IRQ_TIMER) {
     if (!_intr_read()) {
       return context;
     }
