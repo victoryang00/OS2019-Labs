4f443720f71fc6f389f48fb44c595a2c3aa5a41e
tracer-nju <tracer@nju.edu.cn>
2019-05-04 11:06:53 +0800

    Static hostname: doowzs-eve          Icon name: computer-convertible            Chassis: convertible         Machine ID: e1da686fb2fc4789ab3da95481c9bdeb            Boot ID: b97218f761124d6f92f3b4e6f9a88ba1   Operating System: Ubuntu 19.04             Kernel: Linux 4.4.178chromium-g09559f81       Architecture: x86-64  11:06:53 up 21:38,  5 users,  load average: 0.84, 1.62, 1.67

--- kernel/src/thread.c
+++ kernel/src/thread.c
@@ -131,10 +131,10 @@ _Context *kmt_context_switch(_Event ev, _Context *context) {
 
 struct task *kmt_sched() {
   Assert(spinlock_holding(&task_lock), "Not holding the task lock!");
-  //Log("========== TASKS ==========");
+  Log("========== TASKS ==========");
   struct task *ret = NULL;
   for (struct task *tp = &root_task; tp != NULL; tp = tp->next) {
-    //Log("%d:%s [%s]", tp->pid, tp->name, task_states_human[tp->state]);
+    Log("%d:%s [%s]", tp->pid, tp->name, task_states_human[tp->state]);
     if (tp->state == ST_E || tp->state == ST_W) {  // choose a waken up task
       if (ret == NULL || tp->count < ret->count) { // a least ran one
         ret = tp;
@@ -142,7 +142,7 @@ struct task *kmt_sched() {
       }
     }
   }
-  //Log("===========================");
+  Log("===========================");
   return ret;
 }
 _Context *kmt_yield(_Event ev, _Context *context) {
