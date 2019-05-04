153cc1475e712ca73f6f84fa0c8570bea6a58ea4
tracer-nju <tracer@nju.edu.cn>
2019-05-04 19:16:03 +0800

    Static hostname: doowzs-eve          Icon name: computer-convertible            Chassis: convertible         Machine ID: e1da686fb2fc4789ab3da95481c9bdeb            Boot ID: dc30a845a7a5457ab929d09156af8f15   Operating System: Ubuntu 19.04             Kernel: Linux 4.4.178chromium-g09559f81       Architecture: x86-64  19:16:03 up 57 min,  5 users,  load average: 2.00, 2.75, 2.29

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -90,7 +90,10 @@ static _Context *os_trap(_Event ev, _Context *context) {
     Panic("BAD EVENT %d: %s, caused by (%p of %p)", ev.event, ev.msg, ev.cause, ev.ref);
   }
   CLog(BG_CYAN, "Event %d: %s", ev.event, ev.msg);
-  if (ev.event == _EVENT_IRQ_IODEV) printf("\nIODEV IRQ on CPU %d\n", _cpu());
+  if (ev.event == _EVENT_IRQ_IODEV) {
+    printf("\nIODEV IRQ on CPU %d\n", _cpu());
+    printf("context at %p\n", context);
+  }
 
   Assert(!spinlock_holding(&os_trap_lock), "trap in trap!");
   spinlock_acquire(&os_trap_lock);
