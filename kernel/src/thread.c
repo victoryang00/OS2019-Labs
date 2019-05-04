97a0d8a20e3b3c0b26843e52f24b84496e4f7b73
tracer-nju <tracer@nju.edu.cn>
2019-05-04 16:37:08 +0800

    Static hostname: doowzs-eve          Icon name: computer-convertible            Chassis: convertible         Machine ID: e1da686fb2fc4789ab3da95481c9bdeb            Boot ID: b97218f761124d6f92f3b4e6f9a88ba1   Operating System: Ubuntu 19.04             Kernel: Linux 4.4.178chromium-g09559f81       Architecture: x86-64  16:37:08 up 1 day,  3:08,  5 users,  load average: 1.13, 1.67, 2.30

--- kernel/src/thread.c
+++ kernel/src/thread.c
@@ -128,6 +128,7 @@ _Context *kmt_context_save(_Event ev, _Context *context) {
   spinlock_release(&task_lock);
   return NULL;
 }
+
 _Context *kmt_context_switch(_Event ev, _Context *context) {
   //Log("KMT Context Switch");
   spinlock_acquire(&task_lock);
@@ -139,7 +140,6 @@ _Context *kmt_context_switch(_Event ev, _Context *context) {
     cur->context = NULL;
     Log("Context for task %d: %s loaded.", cur->pid, cur->name);
   } else {
-    printf("N\n");
     ret = null_context;
   }
   spinlock_release(&task_lock);
