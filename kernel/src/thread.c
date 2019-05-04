badcfb2dd5abdf38287b5e82d746f6869f5c081b
tracer-nju <tracer@nju.edu.cn>
2019-05-04 22:26:23 +0800

    Static hostname: doowzs-eve          Icon name: computer-convertible            Chassis: convertible         Machine ID: e1da686fb2fc4789ab3da95481c9bdeb            Boot ID: dc30a845a7a5457ab929d09156af8f15   Operating System: Ubuntu 19.04             Kernel: Linux 4.4.178chromium-g09559f81       Architecture: x86-64  22:26:23 up  4:08,  5 users,  load average: 6.51, 9.19, 6.14

--- kernel/src/thread.c
+++ kernel/src/thread.c
@@ -176,7 +176,7 @@ _Context *kmt_yield(_Event ev, _Context *context) {
     if (cur) {
       cur->count = cur->count >= 1000 ? 0 : cur->count + 1;
       cur->state = ST_R;
-      ret = cur->context; // >?
+      ret = cur->context;
     }
   } else {
     Log("Switching to task %d:%s", next->pid, next->name);
