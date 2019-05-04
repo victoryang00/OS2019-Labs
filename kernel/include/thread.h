cd8ec95dd0d6eee7163f960d3184bd103dbf25cc
tracer-nju <tracer@nju.edu.cn>
2019-05-05 00:43:16 +0800

    Static hostname: doowzs-eve          Icon name: computer-convertible            Chassis: convertible         Machine ID: e1da686fb2fc4789ab3da95481c9bdeb            Boot ID: dc30a845a7a5457ab929d09156af8f15   Operating System: Ubuntu 19.04             Kernel: Linux 4.4.178chromium-g09559f81       Architecture: x86-64  00:43:16 up  6:24,  6 users,  load average: 1.82, 1.60, 1.35

--- kernel/include/thread.h
+++ kernel/include/thread.h
@@ -54,6 +54,7 @@ _Context *kmt_context_save(_Event, _Context *);
 _Context *kmt_context_switch(_Event, _Context *);
 struct task *kmt_sched();
 _Context *kmt_yield(_Event, _Context *);
+void kmt_before_sleep(struct task *);
 void kmt_sleep(void *, struct spinlock *);
 void kmt_wakeup(void *);
 
