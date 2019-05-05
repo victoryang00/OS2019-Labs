6a20b3609e1a7414007590e1f6e2431d2ec0448d
tracer-nju <tracer@nju.edu.cn>
2019-05-05 11:28:05 +0800

    Static hostname: doowzs-eve          Icon name: computer-convertible            Chassis: convertible         Machine ID: e1da686fb2fc4789ab3da95481c9bdeb            Boot ID: dc30a845a7a5457ab929d09156af8f15   Operating System: Ubuntu 19.04             Kernel: Linux 4.4.178chromium-g09559f81       Architecture: x86-64  11:28:05 up 17:09,  6 users,  load average: 0.31, 0.70, 1.10

--- kernel/src/semaphore.c
+++ kernel/src/semaphore.c
@@ -13,9 +13,7 @@ void semaphore_init(struct semaphore *sem, const char *name, int value) {
 
 void semaphore_wait(struct semaphore *sem) {
   spinlock_acquire(&sem->lock);
-  //Log("Waiting for semaphore %s (%d)", sem->name, sem->value);
   while (sem->value <= 0) {
-    // SYSCALL: go to sleep
     spinlock_release(&sem->lock);
     asm volatile ("int $0x80" : : "a"(SYS_sem_wait), "b"(sem));
     spinlock_acquire(&sem->lock);
@@ -28,6 +26,5 @@ void semaphore_signal(struct semaphore *sem) {
   spinlock_acquire(&sem->lock);
   ++sem->value;
   asm volatile ("int $0x80" : : "a"(SYS_sem_signal), "b"(sem));
-  //Log("Signaled for semaphore %s (%d)", sem->name, sem->value);
   spinlock_release(&sem->lock);
 }
