b779cdc5cefc28b3937698d77ea5b06d5afdd4df
tracer-nju <tracer@nju.edu.cn>
2019-05-05 10:47:21 +0800

    Static hostname: doowzs-eve          Icon name: computer-convertible            Chassis: convertible         Machine ID: e1da686fb2fc4789ab3da95481c9bdeb            Boot ID: dc30a845a7a5457ab929d09156af8f15   Operating System: Ubuntu 19.04             Kernel: Linux 4.4.178chromium-g09559f81       Architecture: x86-64  10:47:21 up 16:29,  6 users,  load average: 1.47, 1.15, 1.32

--- kernel/src/semaphore.c
+++ kernel/src/semaphore.c
@@ -6,7 +6,7 @@
 #include <debug.h>
 
 void semaphore_init(struct semaphore *sem, const char *name, int value) {
-  spinlock_init(&sem->lock, strcat("Sem Lock of ", name));
+  spinlock_init(&sem->lock, "Sem Lock");
   sem->name = name;
   sem->value = value;
 }
