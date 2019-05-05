4b990d4ca40630fa872bb6761c12fbae0d6eb435
tracer-nju <tracer@nju.edu.cn>
2019-05-05 11:55:06 +0800

    Static hostname: doowzs-eve          Icon name: computer-convertible            Chassis: convertible         Machine ID: e1da686fb2fc4789ab3da95481c9bdeb            Boot ID: dc30a845a7a5457ab929d09156af8f15   Operating System: Ubuntu 19.04             Kernel: Linux 4.4.178chromium-g09559f81       Architecture: x86-64  11:55:06 up 17:36,  6 users,  load average: 2.44, 2.38, 2.03

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -60,7 +60,7 @@ static void os_init() {
   kmt->sem_init(&sem_p, "Producer SEM", 5);
   kmt->sem_init(&sem_c, "Customer SEM", 0);
   kmt->sem_init(&mutex, "Producer-Customer MUTEX", 1);
-  for (int i = 0; i < 1; ++i) {
+  for (int i = 0; i < 2; ++i) {
     kmt->create(pmm->alloc(sizeof(task_t)), "Producer Task", producer, NULL);
     kmt->create(pmm->alloc(sizeof(task_t)), "Customer Task", customer, NULL);
   }
