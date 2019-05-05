f823bf5601a0f7a72ed6bc694c8a9dcc71c5544c
tracer-nju <tracer@nju.edu.cn>
2019-05-05 14:11:42 +0800

    Static hostname: doowzs-eve          Icon name: computer-convertible            Chassis: convertible         Machine ID: e1da686fb2fc4789ab3da95481c9bdeb            Boot ID: dc30a845a7a5457ab929d09156af8f15   Operating System: Ubuntu 19.04             Kernel: Linux 4.4.178chromium-g09559f81       Architecture: x86-64  14:11:42 up 19:53,  7 users,  load average: 3.14, 4.36, 3.24

--- kernel/src/thread.c
+++ kernel/src/thread.c
@@ -161,7 +161,7 @@ struct task *kmt_sched() {
   Log("========== TASKS ==========");
   struct task *ret = NULL;
   printf("\ninterrupt by cpu %d\n", _cpu());
-  if ((struct task *cur = get_current_task()) != NULL) {
+  if ((struct task *cur, cur = get_current_task()) != NULL) {
     printf("interrupter task is %d:%s", cur->pid, cur->name);
   }
   for (struct task *tp = &root_task; tp != NULL; tp = tp->next) {
