13972417274f245d5bc67d21b8a31666446f01fd
tracer-nju <tracer@nju.edu.cn>
2019-05-06 18:14:59 +0800

    Static hostname: doowzs-eve          Icon name: computer-convertible            Chassis: convertible         Machine ID: e1da686fb2fc4789ab3da95481c9bdeb            Boot ID: 1ba2241bcb60422e8c3d0b47714943a0   Operating System: Ubuntu 19.04             Kernel: Linux 4.4.178chromium-g09559f81       Architecture: x86-64  18:14:59 up  4:32,  6 users,  load average: 2.31, 2.58, 2.01

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -86,8 +86,8 @@ static void os_init() {
 
   kmt->create(pmm->alloc(sizeof(task_t)), "print", echo_task, "tty1");
   kmt->create(pmm->alloc(sizeof(task_t)), "print", echo_task, "tty2");
-  kmt->create(pmm->alloc(sizeof(task_t)), "print", echo_task, "tty3");
-  kmt->create(pmm->alloc(sizeof(task_t)), "print", echo_task, "tty4");
+  //kmt->create(pmm->alloc(sizeof(task_t)), "print", echo_task, "tty3");
+  //kmt->create(pmm->alloc(sizeof(task_t)), "print", echo_task, "tty4");
 }
 
 static void os_run() {
