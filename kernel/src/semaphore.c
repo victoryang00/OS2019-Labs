a54c384d4faf57f3b969e792c5f1bd1700a2799d
tracer-nju <tracer@nju.edu.cn>
2019-05-06 21:08:13 +0800

    Static hostname: doowzs-eve          Icon name: computer-convertible            Chassis: convertible         Machine ID: e1da686fb2fc4789ab3da95481c9bdeb            Boot ID: 1ba2241bcb60422e8c3d0b47714943a0   Operating System: Ubuntu 19.04             Kernel: Linux 4.4.178chromium-g09559f81       Architecture: x86-64  21:08:13 up  7:25,  6 users,  load average: 1.09, 0.95, 1.11

--- kernel/src/semaphore.c
+++ kernel/src/semaphore.c
@@ -15,14 +15,14 @@ void semaphore_init(struct semaphore *sem, const char *name, int value) {
 
 void semaphore_wait(struct semaphore *sem) {
   spinlock_acquire(&sem->lock);
-  printf("-[%s = %d]\n", sem->name, sem->value);
+  //printf("-[%s = %d]\n", sem->name, sem->value);
   while (sem->value <= 0) {
     // hold the lock so that it will not be interrupted
     spinlock_acquire(&goto_sleep_lock);
     spinlock_release(&sem->lock);
     asm volatile ("int $0x80" : : "a"(SYS_sem_wait), "b"(sem), "c"(&sem->lock));
     spinlock_acquire(&sem->lock);
-    printf("-[%s = %d]\n", sem->name, sem->value);
+    //printf("-[%s = %d]\n", sem->name, sem->value);
   }
   __sync_synchronize();
   --sem->value;
@@ -33,7 +33,7 @@ void semaphore_signal(struct semaphore *sem) {
   spinlock_acquire(&sem->lock);
   ++sem->value;
   __sync_synchronize();
-  printf("+[%s = %d]\n", sem->name, sem->value);
+  //printf("+[%s = %d]\n", sem->name, sem->value);
   asm volatile ("int $0x80" : : "a"(SYS_sem_signal), "b"(sem));
   spinlock_release(&sem->lock);
 }
