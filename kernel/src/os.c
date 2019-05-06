654422123340ce0aa29a313915f2bb9f58dbc4f7
tracer-nju <tracer@nju.edu.cn>
2019-05-06 19:15:24 +0800

    Static hostname: doowzs-eve          Icon name: computer-convertible            Chassis: convertible         Machine ID: e1da686fb2fc4789ab3da95481c9bdeb            Boot ID: 1ba2241bcb60422e8c3d0b47714943a0   Operating System: Ubuntu 19.04             Kernel: Linux 4.4.178chromium-g09559f81       Architecture: x86-64  19:15:24 up  5:32,  6 users,  load average: 3.12, 2.57, 2.15

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -97,7 +97,7 @@ static void os_run() {
 
 static _Context *os_trap(_Event ev, _Context *context) {
   CLog(BG_CYAN, "Event %d: %s", ev.event, ev.msg);
-  if (ev.event = _EVENT_IRQ_IODEV) printf("K");
+  if (ev.event == _EVENT_IRQ_IODEV) printf("K");
 
   bool holding = spinlock_holding(&os_trap_lock);
   if (!holding) spinlock_acquire(&os_trap_lock);
