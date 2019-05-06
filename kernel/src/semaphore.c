b35bc34d4fbed3e235ad86da97f868e41246a8a3
tracer-nju <tracer@nju.edu.cn>
2019-05-07 01:29:02 +0800

    Static hostname: doowzs-eve          Icon name: computer-convertible            Chassis: convertible         Machine ID: e1da686fb2fc4789ab3da95481c9bdeb            Boot ID: 1ba2241bcb60422e8c3d0b47714943a0   Operating System: Ubuntu 19.04             Kernel: Linux 4.4.178chromium-g09559f81       Architecture: x86-64  01:29:02 up 11:46,  6 users,  load average: 0.21, 0.38, 0.83

--- kernel/src/semaphore.c
+++ kernel/src/semaphore.c
@@ -14,6 +14,7 @@ void semaphore_init(struct semaphore *sem, const char *name, int value) {
 void semaphore_wait(struct semaphore *sem) {
   spinlock_acquire(&sem->lock);
   while (sem->value <= 0) {
+    CLog(FG_YELLOW, "goto sleep");
     asm volatile ("int $0x80" : : "a"(SYS_sleep), "b"(sem), "c"(&sem->lock));
   }
   Assert(spinlock_holding(&sem->lock), "Not holding the lock after waking up");
@@ -23,13 +24,9 @@ void semaphore_wait(struct semaphore *sem) {
 }
 
 void semaphore_signal(struct semaphore *sem) {
-  printf("before signal on CPU %d, will acquire lock %s\n", _cpu(), sem->lock.name);
   spinlock_acquire(&sem->lock);
-  printf("signal lock acquired on CPU %d\n", _cpu());
   ++sem->value;
   __sync_synchronize();
   asm volatile ("int $0x80" : : "a"(SYS_wakeup), "b"(sem));
-  printf("signal OK of CPU %d\n", _cpu());
   spinlock_release(&sem->lock);
-  printf("signal lock %s released on CPU %d\n", sem->lock.name, _cpu());
 }
