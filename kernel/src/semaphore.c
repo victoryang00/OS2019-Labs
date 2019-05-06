c6a2d3d27cbbc8a88c39ca961837282032070366
tracer-nju <tracer@nju.edu.cn>
2019-05-07 01:51:44 +0800

    Static hostname: doowzs-eve          Icon name: computer-convertible            Chassis: convertible         Machine ID: e1da686fb2fc4789ab3da95481c9bdeb            Boot ID: 1ba2241bcb60422e8c3d0b47714943a0   Operating System: Ubuntu 19.04             Kernel: Linux 4.4.178chromium-g09559f81       Architecture: x86-64  01:51:44 up 12:08,  6 users,  load average: 0.58, 0.72, 0.80

--- kernel/src/semaphore.c
+++ kernel/src/semaphore.c
@@ -14,10 +14,8 @@ void semaphore_init(struct semaphore *sem, const char *name, int value) {
 void semaphore_wait(struct semaphore *sem) {
   spinlock_acquire(&sem->lock);
   while (sem->value <= 0) {
-    asm volatile ("int $0x80" 
-      : 
-      : "0"(SYS_sleep), "b"(sem), "c"(&sem->lock)
-    );
+    asm volatile ("int $0x80" : : "a"(SYS_sleep), "b"(sem), "c"(&sem->lock));
+    __sync_synchronize();
   }
   Assert(spinlock_holding(&sem->lock), "Not holding the lock after waking up");
   __sync_synchronize();
