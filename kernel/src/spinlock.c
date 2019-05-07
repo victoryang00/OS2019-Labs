2e9fb9fe5e36a5c2b5aebc5437fb8be1e46cee85
tracer-nju <tracer@nju.edu.cn>
2019-05-07 15:39:52 +0800

    Static hostname: doowzs-eve          Icon name: computer-convertible            Chassis: convertible         Machine ID: e1da686fb2fc4789ab3da95481c9bdeb            Boot ID: d48dda1e42f3489795c43171ee98cf9a   Operating System: Ubuntu 19.04             Kernel: Linux 4.4.178chromium-g09559f81       Architecture: x86-64  15:39:52 up  1:38,  5 users,  load average: 1.71, 1.84, 1.82

--- kernel/src/spinlock.c
+++ kernel/src/spinlock.c
@@ -32,8 +32,6 @@ void spinlock_acquire(struct spinlock *lk) {
   while (1) {
     if (_atomic_xchg((intptr_t *) &lk->locked, 1) == 0) break;
     pause();
-    pause();
-    pause();
   }
   __sync_synchronize();
 
