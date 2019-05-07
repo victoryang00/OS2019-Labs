934c50881518a946f9d13e02d16dee5d4c226b88
tracer-nju <tracer@nju.edu.cn>
2019-05-07 16:47:03 +0800

    Static hostname: doowzs-eve          Icon name: computer-convertible            Chassis: convertible         Machine ID: e1da686fb2fc4789ab3da95481c9bdeb            Boot ID: d48dda1e42f3489795c43171ee98cf9a   Operating System: Ubuntu 19.04             Kernel: Linux 4.4.178chromium-g09559f81       Architecture: x86-64  16:47:03 up  2:45,  6 users,  load average: 1.41, 2.36, 2.42

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -44,8 +44,7 @@ void echo_task(void *name) {
   device_t *tty = dev_lookup(name);
   char text[128], line[128];
   while (1) {
-    sprintf(text, "(%s) $", name);
-    tty->ops->write(tty, 0, text, strlen(text));
+    tty->ops->write(tty, 0, "HELLO", 5);
 
     int nread = tty->ops->read(tty, 0, line, 128);
     line[nread - 1] = '\0';
