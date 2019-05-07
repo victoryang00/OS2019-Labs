a8d7c91c70fac80b4ffece7771fde9ee9fe30299
tracer-nju <tracer@nju.edu.cn>
2019-05-07 21:10:48 +0800

    Static hostname: doowzs-eve          Icon name: computer-convertible            Chassis: convertible         Machine ID: e1da686fb2fc4789ab3da95481c9bdeb            Boot ID: d48dda1e42f3489795c43171ee98cf9a   Operating System: Ubuntu 19.04             Kernel: Linux 4.4.178chromium-g09559f81       Architecture: x86-64  21:10:48 up  7:09,  6 users,  load average: 2.33, 1.81, 1.10

--- kernel/src/thread.c
+++ kernel/src/thread.c
@@ -229,7 +229,7 @@ uintptr_t kmt_sleep(void *alarm, struct spinlock *lock) {
     CLog(FG_YELLOW, "No sleep");
     cur->state = ST_W;
     set_current_task(kmt_sched());
-    printf("[%d] switched to %s", get_current_task() ? get_current_task()->name : "NULL");
+    printf("[%d] switched to %s\n", get_current_task() ? get_current_task()->name : "NULL");
     return -1;
   } else {
     cur->state = ST_S;
