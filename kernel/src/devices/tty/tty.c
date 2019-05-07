617b20ff9da168e5ffec4710d77364eb3f5c8ff9
tracer-nju <tracer@nju.edu.cn>
2019-05-07 16:57:23 +0800

    Static hostname: doowzs-eve          Icon name: computer-convertible            Chassis: convertible         Machine ID: e1da686fb2fc4789ab3da95481c9bdeb            Boot ID: d48dda1e42f3489795c43171ee98cf9a   Operating System: Ubuntu 19.04             Kernel: Linux 4.4.178chromium-g09559f81       Architecture: x86-64  16:57:23 up  2:56,  6 users,  load average: 0.11, 1.03, 1.85

--- kernel/src/devices/tty/tty.c
+++ kernel/src/devices/tty/tty.c
@@ -90,7 +90,6 @@ static void tty_render(tty_t *tty) {
   struct character *ch = tty->buf;
   uint8_t *d = tty->dirty;
   kmt->sem_wait(&tty->lock);
-  //printf(">01 tty lock acquired\n");
   for (int y = 0; y < tty->lines; y++) {
     for (int x = 0; x < tty->columns; x++) {
       if (*d) {
@@ -107,7 +106,6 @@ static void tty_render(tty_t *tty) {
     }
   }
   kmt->sem_signal(&tty->lock);
-  //printf(">02 tty lock released\n");
 }
 
 static void tty_mark(tty_t *tty, struct character *ch) {
@@ -211,14 +209,11 @@ ssize_t tty_read(device_t *dev, off_t offset, void *buf, size_t count) {
 ssize_t tty_write(device_t *dev, off_t offset, const void *buf, size_t count) {
   tty_t *tty = dev->ptr;
   kmt->sem_wait(&tty->lock);
-  printf("=>1 write lock acquired\n");
   for (size_t i = 0; i < count; i++) {
     tty_putc(tty, ((const char *)buf)[i]);
   }
   kmt->sem_signal(&tty->lock);
-  printf("=>2 write lock released\n");
   tty_render(tty);
-  printf("=>3 tty rendered\n");
   return count;
 }
 
--- kernel/src/os.c
+++ kernel/src/os.c
@@ -44,11 +44,11 @@ void echo_task(void *name) {
   device_t *tty = dev_lookup(name);
   char text[128], line[128];
   while (1) {
-    tty->ops->write(tty, 0, "HELLO", 5);
     int nread = tty->ops->read(tty, 0, line, 128);
     line[nread - 1] = '\0';
     sprintf(text, "Echo: %s.\n(%s) $ ", line, name);
     tty->ops->write(tty, 0, text, strlen(text));
+    tty->ops->write(tty, 0, "HELLO", 5);
   }
 }
 
