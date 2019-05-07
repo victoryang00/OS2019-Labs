1129ac5d6e43f8686f21166281cd7f2c3f979b65
tracer-nju <tracer@nju.edu.cn>
2019-05-07 17:05:24 +0800

    Static hostname: doowzs-eve          Icon name: computer-convertible            Chassis: convertible         Machine ID: e1da686fb2fc4789ab3da95481c9bdeb            Boot ID: d48dda1e42f3489795c43171ee98cf9a   Operating System: Ubuntu 19.04             Kernel: Linux 4.4.178chromium-g09559f81       Architecture: x86-64  17:05:25 up  3:04,  7 users,  load average: 0.24, 0.45, 1.23

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -43,13 +43,12 @@ struct spinlock sprintf_lock;
 void echo_task(void *name) {
   device_t *tty = dev_lookup(name);
   char text[128], line[128];
+  int nread = tty->ops->read(tty, 0, line, 128);
+  line[nread - 1] = '\0';
+  sprintf(text, "Echo: %s.\n(%s) $ ", line, name);
+  tty->ops->write(tty, 0, text, strlen(text));
   tty->ops->write(tty, 0, "HELLO", 5);
-  while (1) {
-    int nread = tty->ops->read(tty, 0, line, 128);
-    line[nread - 1] = '\0';
-    sprintf(text, "Echo: %s.\n(%s) $ ", line, name);
-    tty->ops->write(tty, 0, text, strlen(text));
-  }
+  while (1) { pause(); }
 }
 
 static void os_init() {
