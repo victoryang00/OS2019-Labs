515ca3b20a3536551814850d08aa59800c4ea713
tracer-nju <tracer@nju.edu.cn>
2019-05-07 16:00:16 +0800

    Static hostname: doowzs-eve          Icon name: computer-convertible            Chassis: convertible         Machine ID: e1da686fb2fc4789ab3da95481c9bdeb            Boot ID: d48dda1e42f3489795c43171ee98cf9a   Operating System: Ubuntu 19.04             Kernel: Linux 4.4.178chromium-g09559f81       Architecture: x86-64  16:00:16 up  1:59,  5 users,  load average: 0.48, 0.84, 1.30

--- kernel/src/syscall.c
+++ kernel/src/syscall.c
@@ -5,21 +5,25 @@
 
 _Context* do_syscall(_Event ev, _Context *context) {
   Assert(ev.event == _EVENT_SYSCALL, "not a syscall");
-  Assert(context->eax != -1, "bad eax for syscall");
+  Assert(context->GPR1 != -1, "bad eax for syscall");
   
-  uintptr_t a[4];
+  uintptr_t a[4] = {};
   a[0] = context->GPR1;
-  a[1] = context->ebx;
-  a[2] = context->ecx;
-  a[3] = context->edx;
+  a[1] = context->GPR2;
+  a[2] = context->GPR3;
+  a[3] = context->GPR4;
   uintptr_t ret = 0;
 
   switch (a[0]) {
     case SYS_sleep:
+      CLog(FG_YELLOW, "goto sleep");
       ret = kmt_sleep((void *) a[1], (struct spinlock *) a[2]);
+      CLog(FG_YELLOW, "already sleep");
       break;
     case SYS_wakeup:
+      CLog(FG_YELLOW, "goto wakeup");
       ret = kmt_wakeup((void *) a[1]);
+      CLog(FG_YELLOW, "already wakeup");
       break;
     default: Panic("Unhandled syscall ID = %d", a[0]);
   }
