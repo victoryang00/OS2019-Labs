72aa781380bd91753191a4f590e834fc6c38e609
tracer-nju <tracer@nju.edu.cn>
2019-05-07 23:38:33 +0800

    Static hostname: doowzs-eve          Icon name: computer-convertible            Chassis: convertible         Machine ID: e1da686fb2fc4789ab3da95481c9bdeb            Boot ID: 4757d0a25f304a0aa42fb663ca3d1164   Operating System: Ubuntu 19.04             Kernel: Linux 5.0.0-13-generic       Architecture: x86-64  23:38:33 up  1:36,  5 users,  load average: 0.10, 0.77, 1.32

--- kernel/src/thread.c
+++ kernel/src/thread.c
@@ -118,11 +118,11 @@ _Context *kmt_context_save(_Event ev, _Context *context) {
     cur->state   = ST_W;
     cur->owner   = -1;
     cur->context = context;
-    Assert(context->eip > 0x000010, "bad eip (1) when saving %s", cur->name);
+    Assert(context->eip > 0x100000, "bad eip (1) when saving %s", cur->name);
     Assert(context->eip < 0x200000, "bad eip (2) when saving %s", cur->name);
   } else {
     Assert(!null_contexts[_cpu()], "double context saving for null context");
-    Assert(context->eip > 0x000010, "bad eip (1) when saving null");
+    Assert(context->eip > 0x100000, "bad eip (1) when saving null");
     Assert(context->eip < 0x200000, "bad eip (2) when saving null");
     null_contexts[_cpu()] = context;
   }
@@ -141,7 +141,7 @@ _Context *kmt_context_switch(_Event ev, _Context *context) {
     ret = cur->context;
     cur->context = NULL;
     Assert(ret, "task context is empty");
-    Assert(ret->eip > 0x000010, "bad eip (1) when switching to %s", cur->name);
+    Assert(ret->eip > 0x100000, "bad eip (1) when switching to %s", cur->name);
     Assert(ret->eip < 0x200000, "bad eip (2) when switching to %s", cur->name);
     if (cur->alarm) {
       Assert(cur->lock, "No lock to reacquire");
@@ -155,7 +155,7 @@ _Context *kmt_context_switch(_Event ev, _Context *context) {
     ret = null_contexts[_cpu()];
     null_contexts[_cpu()] = NULL;
     Assert(ret, "null context is empty");
-    Assert(ret->eip > 0x000010, "bad eip (1) when switching to null");
+    Assert(ret->eip > 0x100000, "bad eip (1) when switching to null");
     Assert(ret->eip < 0x200000, "bad eip (2) when switching to null");
   }
   return ret;
