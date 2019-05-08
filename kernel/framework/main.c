9b860eb2266d7f29bce6a3cce9fefdadc9e8771e
tracer-nju <tracer@nju.edu.cn>
2019-05-08 23:47:42 +0800

    Static hostname: doowzs-vmbox          Icon name: computer-vm            Chassis: vm         Machine ID: 71088568831446748f7bb3a5439058a5            Boot ID: 6174b26144884a0aa5e7859d578f8c8f     Virtualization: oracle   Operating System: Ubuntu 19.04             Kernel: Linux 5.0.0-13-generic       Architecture: x86-64  23:47:42 up  3:29,  5 users,  load average: 0.06, 0.06, 0.24

--- kernel/framework/main.c
+++ kernel/framework/main.c
@@ -1,5 +1,21 @@
+#include <common.h>
 #include <kernel.h>
 #include <klib.h>
+
+void echo_task(void *arg) {
+  char *name = (char *) arg;
+  char text[128] = "", line[128] = "";
+  device_t *tty = device_lookup(name);
+  while (1) {
+    sprintf(text, "(%s) $ ", name);
+    tty->ops->write(tty, 0, text, strlen(text));
+
+    int nread = tty->ops->read(tty, 0, line, sizeof(line));
+    line[nread - 1] = '\0';
+    sprintf(text, "Echo: %s.\n", line);
+    tty->ops->write(tty, 0, text, strlen(text));
+  }
+}
 
 int main() {
   _ioe_init();
@@ -7,6 +23,12 @@ int main() {
 
   // call sequential init code
   os->init();
+
+  kmt->create(pmm->alloc(sizeof(task_t)), "echo-1", echo_task, "tty1");
+  kmt->create(pmm->alloc(sizeof(task_t)), "echo-2", echo_task, "tty2");
+  kmt->create(pmm->alloc(sizeof(task_t)), "echo-3", echo_task, "tty3");
+  kmt->create(pmm->alloc(sizeof(task_t)), "echo-4", echo_task, "tty4");
+
   _mpe_init(os->run); // all cores call os->run()
   return 1;
 }
