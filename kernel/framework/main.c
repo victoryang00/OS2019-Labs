8e8aa49b144de91cf6d3e4c2afe09c10449fd006
tracer-nju <tracer@nju.edu.cn>
2019-05-08 20:09:41 +0800

    Static hostname: doowzs-vmbox          Icon name: computer-vm            Chassis: vm         Machine ID: 71088568831446748f7bb3a5439058a5            Boot ID: 668cabd700a046c9b8d966564e438f8e     Virtualization: oracle   Operating System: Ubuntu 19.04             Kernel: Linux 5.0.0-13-generic       Architecture: x86-64  20:09:41 up 16 min,  5 users,  load average: 1.32, 0.85, 0.79

--- kernel/framework/main.c
+++ kernel/framework/main.c
@@ -1,5 +1,28 @@
 #include <kernel.h>
 #include <klib.h>
+#include <devices.h>
+
+sem_t sem_p, sem_c, mutex;
+void producer(void *arg) {
+  device_t *tty = dev_lookup("tty1");
+  while (1) {
+    kmt->sem_wait(&sem_p);
+    kmt->sem_wait(&mutex);
+    tty->ops->write(tty, 0, "(", 1);
+    kmt->sem_signal(&mutex);
+    kmt->sem_signal(&sem_c);
+  }
+}
+void customer(void *arg) {
+  device_t *tty = dev_lookup("tty1");
+  while (1) {
+    kmt->sem_wait(&sem_c);
+    kmt->sem_wait(&mutex);
+    tty->ops->write(tty, 0, ")", 1);
+    kmt->sem_signal(&mutex);
+    kmt->sem_signal(&sem_p);
+  }
+}
 
 int main() {
   _ioe_init();
@@ -7,6 +30,15 @@ int main() {
 
   // call sequential init code
   os->init();
+
+  kmt->sem_init(&sem_p, "producer-sem", 1);
+  kmt->sem_init(&sem_c, "customer-sem", 0);
+  kmt->sem_init(&mutex, "mutex", 1);
+  for (int i = 0; i < 8; ++i) {
+    kmt->create(pmm->alloc(sizeof(task_t)), "p-task", producer, NULL);
+    kmt->create(pmm->alloc(sizeof(task_t)), "c-task", customer, NULL);
+  }
+
   _mpe_init(os->run); // all cores call os->run()
   return 1;
 }
