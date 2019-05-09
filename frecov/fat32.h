a0f1fec4888fa40a5915823b8fd88e599557f1ef
tracer-nju <tracer@nju.edu.cn>
2019-05-09 17:58:20 +0800

    Static hostname: doowzs-vmbox          Icon name: computer-vm            Chassis: vm         Machine ID: 71088568831446748f7bb3a5439058a5            Boot ID: db64132e1105415b95bca0b04ee9fac1     Virtualization: oracle   Operating System: Ubuntu 19.04             Kernel: Linux 5.0.0-13-generic       Architecture: x86-64  17:58:20 up  6:13,  7 users,  load average: 0.59, 0.39, 0.30

--- frecov/fat32.h
+++ frecov/fat32.h
@@ -17,21 +17,21 @@ struct MBR {
   int16_t BPB_FATSz16;      // 0x16 - 0x17
   int16_t BPB_SecPerTrk;    // 0x18 - 0x19
   int16_t BPB_NumHeads;     // 0x1A - 0x1B
-  int64_t BPB_HiddSec;      // 0x1C - 0x1F
-  int64_t BPB_TotSec32;     // 0x20 - 0x23
+  int32_t BPB_HiddSec;      // 0x1C - 0x1F
+  int32_t BPB_TotSec32;     // 0x20 - 0x23
 
   // extended BPB structure for FAT32
-  int64_t BPB_FATSz32;      // 0x24 - 0x27
-  int32_t BPB_ExtFlags;     // 0x28 - 0x29
-  int32_t BPB_FSVer;        // 0x2A - 0x2B
-  int64_t BPB_RootClus;     // 0x2C - 0x2F
-  int32_t BPB_FSInfo;       // 0x30 - 0x31
-  int32_t BPB_BkBootSec;    // 0x32 - 0x33
+  int32_t BPB_FATSz32;      // 0x24 - 0x27
+  int16_t BPB_ExtFlags;     // 0x28 - 0x29
+  int16_t BPB_FSVer;        // 0x2A - 0x2B
+  int32_t BPB_RootClus;     // 0x2C - 0x2F
+  int16_t BPB_FSInfo;       // 0x30 - 0x31
+  int16_t BPB_BkBootSec;    // 0x32 - 0x33
   char    BPB_Reserved[12]; // 0x34 - 0x3F
   int8_t  BS_DrvNum;        // 0x40
   int8_t  BS_Reserved1;     // 0x41
   int8_t  BS_BootSig;       // 0x42
-  int64_t BS_VolID;         // 0x43 - 0x46
+  int32_t BS_VolID;         // 0x43 - 0x46
   char    BS_VolLab[11];    // 0x47 - 0x51
   char    BS_FilSysTyle[8]; // 0x52 - 0x59
   char    EMPTY[420];       // 0x5A
