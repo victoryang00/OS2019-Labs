06f16575b3778aeafa861f5e7f01f9e765342743
tracer-nju <tracer@nju.edu.cn>
2019-05-09 17:00:06 +0800

    Static hostname: doowzs-vmbox          Icon name: computer-vm            Chassis: vm         Machine ID: 71088568831446748f7bb3a5439058a5            Boot ID: db64132e1105415b95bca0b04ee9fac1     Virtualization: oracle   Operating System: Ubuntu 19.04             Kernel: Linux 5.0.0-13-generic       Architecture: x86-64  17:00:06 up  5:15,  7 users,  load average: 0.23, 0.28, 0.21

--- frecov/fat32.h
+++ frecov/fat32.h
@@ -2,22 +2,23 @@
 #define __FAT32_H__
 
 struct MBR {
-  struct __attribute__((packed)) {
-    char jmp_instr[3];      // 00
-    char oem_name[8];       // 03
-    uint16_t byts_per_sec;  // 0B
-    uint8_t sec_per_clus;   // 0D
-    uint16_t rsvd_sec_cnt;  // 0E
-    uint8_t num_fats;       // 10
-    char useless2[0x13];    // 11
-    uint32_t fat_sz_32;     // 24
-    char useless3[0x04];    // 28
-    uint32_t root_clus;     // 2C
-    char useless4[0x18f];   // XX
-  } boot_code;
+  char jmp_instr[3];      // 00
+  char oem_name[8];       // 03
+  char useless;           // 0A
+  uint16_t byts_per_sec;  // 0B
+  uint8_t sec_per_clus;   // 0D
+  uint16_t rsvd_sec_cnt;  // 0E
+  uint8_t num_fats;       // 10
+  char useless2[0x13];    // 11
+  uint32_t fat_sz_32;     // 24
+  char useless3[0x04];    // 28
+  uint32_t root_clus;     // 2C
+  char useless4[0x18f];   // XX
+
   struct __attribute__((packed)) {
     char useless[16];
   } partition[4];
+
   uint16_t signature;
 } __attribute__((packed));
 
