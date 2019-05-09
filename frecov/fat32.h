acf6feebba23fa819a5faabcbb5fce8ce5def934
tracer-nju <tracer@nju.edu.cn>
2019-05-09 17:55:01 +0800

    Static hostname: doowzs-vmbox          Icon name: computer-vm            Chassis: vm         Machine ID: 71088568831446748f7bb3a5439058a5            Boot ID: db64132e1105415b95bca0b04ee9fac1     Virtualization: oracle   Operating System: Ubuntu 19.04             Kernel: Linux 5.0.0-13-generic       Architecture: x86-64  17:55:01 up  6:10,  7 users,  load average: 0.01, 0.12, 0.21

--- frecov/fat32.h
+++ frecov/fat32.h
@@ -8,12 +8,12 @@ struct __attribute__((__packed__)) MBR {
   char    BS_jmpBoot[3];    // 0x00 - 0x02
   char    BS_OEMName[8];    // 0x03 - 0x0A
   int16_t BPB_BytePerSec;   // 0x0B - 0x0C
-  int8_t  BPB_SecPerClus;   // 0x0D
+  char    BPB_SecPerClus;   // 0x0D
   int16_t BPB_RsvdSecCnt;   // 0x0E - 0x0F
-  int8_t  BPB_NumFATs;      // 0x10
+  char    BPB_NumFATs;      // 0x10
   int16_t BPB_RootEntCnt;   // 0x11 - 0x12
   int16_t BPB_TotSec16;     // 0x13 - 0x14
-  int8_t  BPB_Media;        // 0x15
+  char    BPB_Media;        // 0x15
   int16_t BPB_FATSz16;      // 0x16 - 0x17
   int16_t BPB_SecPerTrk;    // 0x18 - 0x19
   int16_t BPB_NumHeads;     // 0x1A - 0x1B
@@ -28,9 +28,9 @@ struct __attribute__((__packed__)) MBR {
   int32_t BPB_FSInfo;       // 0x30 - 0x31
   int32_t BPB_BkBootSec;    // 0x32 - 0x33
   char    BPB_Reserved[12]; // 0x34 - 0x3F
-  int8_t  BS_DrvNum;        // 0x40
-  int8_t  BS_Reserved1;     // 0x41
-  int8_t  BS_BootSig;       // 0x42
+  char    BS_DrvNum;        // 0x40
+  char    BS_Reserved1;     // 0x41
+  char    BS_BootSig;       // 0x42
   int64_t BS_VolID;         // 0x43 - 0x46
   char    BS_VolLab[11];    // 0x47 - 0x51
   char    BS_FilSysTyle[8]; // 0x52 - 0x59
