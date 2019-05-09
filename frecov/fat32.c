f2d227b5ac4ae5f33064f6955fe4d60044d4506e
tracer-nju <tracer@nju.edu.cn>
2019-05-09 21:24:07 +0800

    Static hostname: doowzs-vmbox          Icon name: computer-vm            Chassis: vm         Machine ID: 71088568831446748f7bb3a5439058a5            Boot ID: db64132e1105415b95bca0b04ee9fac1     Virtualization: oracle   Operating System: Ubuntu 19.04             Kernel: Linux 5.0.0-13-generic       Architecture: x86-64  21:24:07 up  9:39,  6 users,  load average: 0.23, 0.16, 0.11

--- frecov/fat32.c
+++ frecov/fat32.c
@@ -19,20 +19,20 @@ struct Disk *disk_load_fat(const char *file) {
 void disk_get_sections(struct Disk *disk) {
   disk->mbr = (struct MBR *) disk->head;
   Assert(disk->mbr->SignatureWord == 0xaa55, "Expecting signature 0xaa55, got 0x%x", disk->mbr->SignatureWord);
-  Log("MBR    at %p, offset 0x%x", disk->mbr, (void *) disk->mbr - disk->head);
+  Log("MBR    at %p, offset 0x%x", disk->mbr, (int) ((void *) disk->mbr - disk->head));
 
   disk->fsinfo = (struct FSInfo *) ((void *) disk->mbr + 512);
-  Log("FSInfo at %p, offset 0x%x", disk->fsinfo, (void *) disk->fsinfo - disk->head);
+  Log("FSInfo at %p, offset 0x%x", disk->fsinfo, (int) ((void *) disk->fsinfo - disk->head));
 
   size_t offst = (size_t) disk->mbr->BPB_BytsPerSec * disk->mbr->BPB_RsvdSecCnt;
   size_t fatsz = (size_t) disk->mbr->BPB_BytsPerSec * disk->mbr->BPB_RsvdSecCnt;
   disk->fat[1] = (struct FAT **) (((void *) disk) + offst);
   disk->fat[2] = (struct FAT **) (((void *) disk->fat[1]) + fatsz);
-  Log("FAT1   at %p, offset 0x%x", disk->fat[1], (void *) disk->fat[1] - disk->head);
-  Log("FAT2   at %p, offset 0x%x", disk->fat[2], (void *) disk->fat[2] - disk->head);
+  Log("FAT1   at %p, offset 0x%x", disk->fat[1], (int) ((void *) disk->fat[1] - disk->head));
+  Log("FAT2   at %p, offset 0x%x", disk->fat[2], (int) ((void *) disk->fat[2] - disk->head));
 
   disk->fdt = (struct FDT **) (((void *) disk->fat[1]) + fatsz * disk->mbr->BPB_NumFATs);
-  Log("DATA   at %p, offset 0x%x", disk->fdt, (void *) disk->fdt - disk->head);
+  Log("DATA   at %p, offset 0x%x", disk->fdt, (int) ((void *) disk->fdt - disk->head));
 }
 
 unsigned char check_sum(unsigned char *c) {
