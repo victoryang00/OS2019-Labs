f43d091f43b2ab57e20f0fc0a28cb4b4c2b322de
tracer-nju <tracer@nju.edu.cn>
2019-05-09 20:43:26 +0800

    Static hostname: doowzs-vmbox          Icon name: computer-vm            Chassis: vm         Machine ID: 71088568831446748f7bb3a5439058a5            Boot ID: db64132e1105415b95bca0b04ee9fac1     Virtualization: oracle   Operating System: Ubuntu 19.04             Kernel: Linux 5.0.0-13-generic       Architecture: x86-64  20:43:26 up  8:58,  7 users,  load average: 0.27, 0.24, 0.19

--- frecov/fat32.c
+++ frecov/fat32.c
@@ -1,17 +1,31 @@
 #include "frecov.h"
 
-void *fat_load(const char *file) {
+struct Disk *disk_load_fat(const char *file) {
+  struct Disk *ret = malloc(sizeof(struct Disk));
+
   int fd = open(file, O_RDONLY);
   Assert(fd, "failed to open file");
 
   struct stat sb;
   Assert(!fstat(fd, &sb), "fstat failed");
-  Log("FILE SIZE IS %lu", sb.st_size);
 
-  void *ret = mmap(NULL, sb.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
-  Assert(ret != MAP_FAILED, "mmap failed");
+  ret->head = mmap(NULL, sb.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
+  Assert(ret->head = MAP_FAILED, "mmap failed");
 
   struct MBR *mbr = (struct MBR *) ret;
   Assert(mbr->SignatureWord == 0xAA55, "bad signature: read 0x%x, expect 0xAA55", mbr->SignatureWord);
+
+  disk_get_sections(ret);
   return ret;
+}
+
+void disk_get_sections(struct Disk *disk) {
+  disk->mbr = (struct MBR *) disk;
+
+  size_t offst = (size_t) disk->mbr->BPB_BytsPerSec * disk->mbr->BPB_RsvdSecCnt;
+  size_t fatsz = (size_t) disk->mbr->BPB_BytsPerSec * disk->mbr->BPB_RsvdSecCnt;
+  disk->fat[1] = (struct FAT **) (((void *) disk) + offst);
+  disk->fat[2] = (struct FAT **) (((void *) disk->fat[1]) + fatsz);
+
+  disk->fdt = (struct FDT **) (((void *) disk->fat[1]) + fatsz * disk->mbr->BPB_NumFATs);
 }
--- frecov/fat32.h
+++ frecov/fat32.h
@@ -7,7 +7,7 @@ struct MBR {
   // common BPB structure
   char     BS_jmpBoot[3];    // 0x00 - 0x02
   char     BS_OEMName[8];    // 0x03 - 0x0A
-  uint16_t BPB_BytePerSec;   // 0x0B - 0x0C
+  uint16_t BPB_BytsPerSec;   // 0x0B - 0x0C
   uint8_t  BPB_SecPerClus;   // 0x0D
   uint16_t BPB_RsvdSecCnt;   // 0x0E - 0x0F
   uint8_t  BPB_NumFATs;      // 0x10
@@ -33,11 +33,58 @@ struct MBR {
   uint8_t  BS_BootSig;       // 0x42
   uint32_t BS_VolID;         // 0x43 - 0x46
   char     BS_VolLab[11];    // 0x47 - 0x51
-  char     BS_FilSysTyle[8]; // 0x52 - 0x59
+  char     BS_FilSysType[8]; // 0x52 - 0x59
   char     EMPTY[420];       // 0x5A
-  uint16_t SignatureWord;     // 0x1FE
+  uint16_t SignatureWord;    // 0x1FE
 } __attribute__((packed));
 
-void *fat_load(const char*);
+struct FAT {
+  union {
+    uint8_t  mark[4];
+    uint32_t next;
+  };
+} __attribute__((packed));
+
+struct FSInfo {
+  uint32_t FSI_LeadSig;        // 0x000
+  char     FSI_Reserved1[480]; // 0x004
+  uint32_t FSI_StrucSig;       // 0x1E4
+  uint32_t FSI_Free_Count;     // 0x1E8
+  uint32_t FSI_Nxt_Free;       // 0x1EC
+  char     FSI_Reserved2[12];  // 0x1F0
+  uint32_t FSI_TrailSig;       // 0x1FE
+} __attribute__((packed));
+
+#define ATTR_READ_ONLY 0x01
+#define ATTR_HIDDEN    0x02
+#define ATTR_SYSTEM    0x04
+#define ATTR_VOLUME_ID 0x08
+#define ATTR_DIRECTORY 0x10
+#define ATTR_ARCHIVE   0x20
+
+struct FDT {
+  char     DIR_Name[11];     // 0x00 - 0x0A
+  uint8_t  DIR_Attr;         // 0x0B
+  uint8_t  DIR_NTRes;        // 0x0C
+  uint8_t  DIR_CrtTimeTenth; // 0x0D
+  uint16_t DIR_CrtTime;      // 0x0E - 0x0F
+  uint16_t DIR_CrtDate;      // 0x10 - 0x11
+  uint16_t DIR_LstAccDate;   // 0x12 - 0x13
+  uint16_t DIR_FstClusHI;    // 0x14 - 0x15
+  uint16_t DIR_WrtTime;      // 0x16 - 0x17
+  uint16_t DIR_WrtDate;      // 0x18 - 0x19
+  uint16_t DIR_FstClusLO;    // 0x1A - 0x1B
+  uint32_t DIR_FileSize;     // 0x1C - 0x1F
+} __attribute__((packed));
+
+struct Disk {
+  void *head;
+  struct MBR *mbr;
+  struct FAT **fat[2];
+  struct FDT **fdt;
+};
+
+struct Disk *disk_load_fat(const char *);
+void disk_get_sections(struct Disk *);
 
 #endif
