f5eead4199fec5ccf163a8bb9108c4e00b4d05ee
tracer-nju <tracer@nju.edu.cn>
2019-05-09 17:29:43 +0800

    Static hostname: doowzs-vmbox          Icon name: computer-vm            Chassis: vm         Machine ID: 71088568831446748f7bb3a5439058a5            Boot ID: db64132e1105415b95bca0b04ee9fac1     Virtualization: oracle   Operating System: Ubuntu 19.04             Kernel: Linux 5.0.0-13-generic       Architecture: x86-64  17:29:43 up  5:45,  7 users,  load average: 0.71, 0.40, 0.31

--- frecov/fat32.c
+++ frecov/fat32.c
@@ -11,9 +11,6 @@ void *fat_load(const char *file) {
   void *ret = mmap(NULL, sb.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
   Assert(ret != MAP_FAILED, "mmap failed");
 
-  struct MBR *mbr = (struct MBR *) ret;
-  Log("OEM Name: %s", mbr->oem_name);
-  Log("offset of signature is %x", (int) ((char *) &mbr->signature - (char *) mbr));
-  Assert(mbr->signature == 0xaa55, "bad MBR signature %x, should be 0xaa55", mbr->signature);
+  Assert(((struct MBR *)ret)->SignatureWord == 0x55AA, "bad signature");
   return ret;
 }
--- frecov/fat32.h
+++ frecov/fat32.h
@@ -1,25 +1,41 @@
 #ifndef __FAT32_H__
 #define __FAT32_H__
 
+#include <stdint.h>
+
 struct MBR {
-  char jmp_instr[3];      // 00
-  char oem_name[8];       // 03
-  char useless;           // 0A
-  uint16_t byts_per_sec;  // 0B
-  uint8_t sec_per_clus;   // 0D
-  uint16_t rsvd_sec_cnt;  // 0E
-  uint8_t num_fats;       // 10
-  char useless2[0x13];    // 11
-  uint32_t fat_sz_32;     // 24
-  char useless3[0x04];    // 28
-  uint32_t root_clus;     // 2C
-  char useless4[0x18d];   // XX
+  // common BPB structure
+  char     BS_jmpBoot[3];    // 0x00 - 0x02
+  char     BS_OEMName[8];    // 0x03 - 0x0A
+  uint16_t BPB_BytePerSec;   // 0x0B - 0x0C
+  uint8_t  BPB_SecPerClus;   // 0x0D
+  uint16_t BPB_RsvdSecCnt;   // 0x0E - 0x0F
+  uint8_t  BPB_NumFATs;      // 0x10
+  uint16_t BPB_RootEntCnt;   // 0x11 - 0x12
+  uint16_t BPB_TotSec16;     // 0x13 - 0x14
+  uint8_t  BPB_Media;        // 0x15
+  uint16_t BPB_FATSz16;      // 0x16 - 0x17
+  uint16_t BPB_SecPerTrk;    // 0x18 - 0x19
+  uint16_t BPB_NumHeads;     // 0x1A - 0x1B
+  uint64_t BPB_HiddSec;      // 0x1C - 0x1F
+  uint64_t BPB_TotSec32;     // 0x20 - 0x23
 
-  struct __attribute__((packed)) {
-    char useless[16];
-  } partition[4];
-
-  uint16_t signature;
+  // extended BPB structure for FAT32
+  uint64_t BPB_FATSz32;      // 0x24 - 0x27
+  uint32_t BPB_ExtFlags;     // 0x28 - 0x29
+  uint32_t BPB_FSVer;        // 0x2A - 0x2B
+  uint64_t BPB_RootClus;     // 0x2C - 0x2F
+  uint32_t BPB_FSInfo;       // 0x30 - 0x31
+  uint32_t BPB_BkBootSec;    // 0x32 - 0x33
+  char     BPB_Reserved[12]; // 0x34 - 0x3F
+  uint8_t  BS_DrvNum;        // 0x40
+  uint8_t  BS_Reserved1;     // 0x41
+  uint8_t  BS_BootSig;       // 0x42
+  uint64_t BS_VolID;         // 0x43 - 0x46
+  char     BS_VolLab[11];    // 0x47 - 0x51
+  char     BS_FilSysTyle[8]; // 0x52 - 0x59
+  char     EMPTY[420];       // 0x5A
+  uint16_t SignatureWord;    // 0x1FE
 } __attribute__((packed));
 
 void *fat_load(const char *);
