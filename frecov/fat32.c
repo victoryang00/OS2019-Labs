3d1b698e9be268d30427e0cd13569d986418158a
tracer-nju <tracer@nju.edu.cn>
2019-05-09 17:32:08 +0800

    Static hostname: doowzs-vmbox          Icon name: computer-vm            Chassis: vm         Machine ID: 71088568831446748f7bb3a5439058a5            Boot ID: db64132e1105415b95bca0b04ee9fac1     Virtualization: oracle   Operating System: Ubuntu 19.04             Kernel: Linux 5.0.0-13-generic       Architecture: x86-64  17:32:08 up  5:47,  7 users,  load average: 0.54, 0.52, 0.36

--- frecov/fat32.c
+++ frecov/fat32.c
@@ -12,7 +12,7 @@ void *fat_load(const char *file) {
   Assert(ret != MAP_FAILED, "mmap failed");
 
   struct MBR *mbr = (struct MBR *) ret;
-  Log("offset of signature is 0x%x", (int) ((void *) &mbr->SignatureWord) - (void *) mbr);
+  Log("offset of signature is 0x%x", (int) ((void *) &mbr->SignatureWord - (void *) mbr));
   Assert(mbr->SignatureWord == 0x55AA, "bad signature: read 0x%x, expect 0x55AA", mbr->SignatureWord);
   return ret;
 }
