5be8b0d91dbbfb8af7cbb5b66c95814b6040e5ba
tracer-nju <tracer@nju.edu.cn>
2019-05-10 00:17:16 +0800

    Static hostname: doowzs-vmbox          Icon name: computer-vm            Chassis: vm         Machine ID: 71088568831446748f7bb3a5439058a5            Boot ID: db64132e1105415b95bca0b04ee9fac1     Virtualization: oracle   Operating System: Ubuntu 19.04             Kernel: Linux 5.0.0-13-generic       Architecture: x86-64  00:17:16 up 12:32,  6 users,  load average: 0.19, 0.24, 0.24

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -49,7 +49,7 @@ bool cluster_is_fdt(void *c, int nr) {
     } else {
       if (fdt_count != 0) return false;
       if (chksum != check_sum((unsigned char *) f[i].name)) {
-      Log("checksum mismatch: %u vs %u", checksum, check_sum((unsigned char *) f[i].name));
+      Log("checksum mismatch: %u vs %u", chksum, check_sum((unsigned char *) f[i].name));
       return false;
       }
     }
