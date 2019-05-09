1badbff4b142554596274dbe982d1b62cd258b0f
tracer-nju <tracer@nju.edu.cn>
2019-05-09 15:31:22 +0800

    Static hostname: doowzs-eve          Icon name: computer-convertible            Chassis: convertible         Machine ID: e1da686fb2fc4789ab3da95481c9bdeb            Boot ID: 96ca2ddefd5843c6974d3280708e9104   Operating System: Ubuntu 19.04             Kernel: Linux 4.4.178chromium-g09559f81       Architecture: x86-64  15:31:22 up 14:41,  6 users,  load average: 0.66, 0.36, 0.27

--- frecov/fat32.c
+++ frecov/fat32.c
@@ -6,9 +6,9 @@ void *fat_load(const char *file) {
 
   struct stat sb;
   Assert(!fstat(fd, &sb), "fstat failed");
-  Log("FILE SIZE IS " FMT_LU, (uint64_t) sb.st_size);
+  Log("FILE SIZE IS %u", sb.st_size);
 
-  void *ret = mmap(NULL, 0, PROT_READ, MAP_PRIVATE, fd, 0);
+  void *ret = mmap(NULL, sb.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
   Assert(ret != MAP_FAILED, "mmap failed");
   return ret;
 }
