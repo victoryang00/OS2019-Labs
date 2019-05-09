75e0f534675159b85bd1a6772c7082cfbe7e8a67
tracer-nju <tracer@nju.edu.cn>
2019-05-09 15:38:52 +0800

    Static hostname: doowzs-eve          Icon name: computer-convertible            Chassis: convertible         Machine ID: e1da686fb2fc4789ab3da95481c9bdeb            Boot ID: 96ca2ddefd5843c6974d3280708e9104   Operating System: Ubuntu 19.04             Kernel: Linux 4.4.178chromium-g09559f81       Architecture: x86-64  15:38:52 up 14:48,  6 users,  load average: 0.41, 0.52, 0.38

--- frecov/fat32.c
+++ frecov/fat32.c
@@ -11,7 +11,7 @@ void *fat_load(const char *file) {
   void *ret = mmap(NULL, sb.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
   Assert(ret != MAP_FAILED, "mmap failed");
 
-  bmr = (BMR *) ret;
+  bmr = (struct BMR *) ret;
   Assert(bmr->signature == 0xaa55, "bad MBR signature %x, should be 0xaa55", bmr->signature);
   return ret;
 }
