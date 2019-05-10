8fc7dc6e750b960317237bf02382fec861872cf9
tracer-nju <tracer@nju.edu.cn>
2019-05-10 10:56:02 +0800

    Static hostname: doowzs-eve          Icon name: computer-convertible            Chassis: convertible         Machine ID: e1da686fb2fc4789ab3da95481c9bdeb            Boot ID: d893866c0427415e906422a140575b9e   Operating System: Ubuntu 19.04             Kernel: Linux 4.4.178chromium-g09559f81       Architecture: x86-64  10:56:02 up 43 min,  6 users,  load average: 0.18, 0.43, 0.41

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -112,7 +112,8 @@ bool handle_fdt(void *c, int nr) {
       copy_name(f + i);
     } else {
       if (f[i].file_size) {
-        printf("%s\n", file_name + pos);
+        int clus = f[i].fst_clus_HI << 16 | f[i].fst_clus_LO;
+        printf("%x -> %s, clus = %d\n", (int) ((void *) f[i] - disk->head), file_name + pos, clus);
       }
       pos = 128;
       file_name[pos] = '\0';
