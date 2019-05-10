752d48bebaa197344d53998d2bb9ba1405a3d266
tracer-nju <tracer@nju.edu.cn>
2019-05-10 12:33:48 +0800

    Static hostname: doowzs-eve          Icon name: computer-convertible            Chassis: convertible         Machine ID: e1da686fb2fc4789ab3da95481c9bdeb            Boot ID: d893866c0427415e906422a140575b9e   Operating System: Ubuntu 19.04             Kernel: Linux 4.4.178chromium-g09559f81       Architecture: x86-64  12:33:48 up  2:21,  6 users,  load average: 0.14, 0.93, 1.11

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -25,7 +25,14 @@ void recover_images() {
   for (void *p = disk->data; p < disk->tail; p += clusz) {
     switch (get_cluster_type(p, nr_clu)) {
       case TYPE_FDT:
-        Log("fdt found at offset %x", (int) (p - disk->head));
+        CLog(FG_BLUE, "fdt found at offset %x", (int) (p - disk->head));
+        struct DataSeg *d = malloc(sizeof(struct DataSeg));
+        d->head = p;
+        d->tail = NULL;
+        d->next = fdt_list.next;
+        d->prev = &fdt_list;
+        fdt_list.next = d;
+        d->next->prev = d;
         for (struct DataSeg *d = fdt_list.next; d != &fdt_list; d = d->next) {
           if (handle_fdt(d->head, nr_clu)) {
             CLog(FG_GREEN, "fdt at %x is handled!", (int) (d->head - disk->head));
@@ -33,16 +40,6 @@ void recover_images() {
             d->next->prev = d->prev;
             free(d);
           }
-        }
-        if (!handle_fdt(p, nr_clu)) {
-          CLog(FG_YELLOW, "fdt at %x is not handled now", (int) (p - disk->head));
-          struct DataSeg *d = malloc(sizeof(struct DataSeg));
-          d->head = p;
-          d->tail = NULL;
-          d->next = fdt_list.next;
-          d->prev = &fdt_list;
-          fdt_list.next = d;
-          d->next->prev = d;
         }
         break;
       case TYPE_BMP:
