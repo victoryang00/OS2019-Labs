49cc2a23ed9f31d8a3febf3e0e4264c8e3e4b2b3
tracer-nju <tracer@nju.edu.cn>
2019-05-10 11:48:20 +0800

    Static hostname: doowzs-eve          Icon name: computer-convertible            Chassis: convertible         Machine ID: e1da686fb2fc4789ab3da95481c9bdeb            Boot ID: d893866c0427415e906422a140575b9e   Operating System: Ubuntu 19.04             Kernel: Linux 4.4.178chromium-g09559f81       Architecture: x86-64  11:48:20 up  1:35,  6 users,  load average: 1.51, 1.44, 1.21

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -116,7 +116,7 @@ bool handle_fdt(void *c, int nr) {
         Assert(0 <= pos && pos < 128, "bad file name");
         //uint32_t clus = ((uint32_t) f[i].fst_clus_HI) << 16 | f[i].fst_clus_LO;
         //printf("%x -> %s, clus = %u\n", (int) ((void *) (f + i) - disk->head), file_name + pos, clus);
-        printf("%s\n", file_name + pos);
+        printf("%x\n", pos);
       }
       pos = 128;
       file_name[pos] = '\0';
