5a40dd20a82bf5bb1027f487771679e072638796
tracer-nju <tracer@nju.edu.cn>
2019-05-16 16:09:19 +0800

    Static hostname: doowzs-eve          Icon name: computer-convertible            Chassis: convertible         Machine ID: e1da686fb2fc4789ab3da95481c9bdeb            Boot ID: 6842f22d09844a2c975a4c563c62ad0c   Operating System: Ubuntu 19.04             Kernel: Linux 4.4.178chromium-g09559f81       Architecture: x86-64  16:09:19 up 2 days,  4:28,  6 users,  load average: 0.14, 0.60, 0.60

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -206,8 +206,7 @@ bool handle_bmp_aux(void *p, size_t sz) {
   return true;
 }
 struct Image *find_best_match(void *p, size_t sz) {
-  int clus = (p - disk->data) / sz + 2;
-  CLog(FG_BLUE, "clus %d", clus);
+  int clus = (p - disk->data) / sz + disk->mbr->BPB_RootClus;
   int16_t *chk = (int16_t *) p;
   int32_t best_diff = 0x3f3f3f3f;
   struct Image *ret = NULL;
