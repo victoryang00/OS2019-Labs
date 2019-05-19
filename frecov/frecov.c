fb0aef22cd89c6a84bbed092677a96d684990385
tracer-nju <tracer@nju.edu.cn>
2019-05-20 00:47:10 +0800

    Static hostname: doowzs-vmbox          Icon name: computer-vm            Chassis: vm         Machine ID: 71088568831446748f7bb3a5439058a5            Boot ID: f2bd2ec33414455cb07431eaf74699b4     Virtualization: oracle   Operating System: Ubuntu 19.04             Kernel: Linux 5.0.0-13-generic       Architecture: x86-64  00:47:10 up 10:56,  6 users,  load average: 0.33, 0.33, 0.34

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -303,6 +303,7 @@ void handle_image(struct Image *image, size_t sz, int nr) {
               uint32_t diff_score = 0;
               if (rgb_down) {
                 for (int dx = 0; dx < 3; ++dx) {
+                  Log("%d", dx);
                   diff_score += rgb_diff(rgb_down + dx * 3, (uint8_t *)clus + dx * 3);
                 }
               } else {
