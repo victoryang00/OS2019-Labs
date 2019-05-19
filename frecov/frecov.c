5e42f77f35507432f99d9459b6881a9a6ca45576
tracer-nju <tracer@nju.edu.cn>
2019-05-20 00:59:10 +0800

    Static hostname: doowzs-vmbox          Icon name: computer-vm            Chassis: vm         Machine ID: 71088568831446748f7bb3a5439058a5            Boot ID: f2bd2ec33414455cb07431eaf74699b4     Virtualization: oracle   Operating System: Ubuntu 19.04             Kernel: Linux 5.0.0-13-generic       Architecture: x86-64  00:59:10 up 11:08,  6 users,  load average: 0.07, 0.46, 0.50

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -293,6 +293,7 @@ void handle_image(struct Image *image, size_t sz, int nr) {
             for (struct DataSeg *dp = bmp_list[i][j][k].next; dp != &bmp_list[i][j][k]; dp = dp->next) {
               if (dp->holder == image) continue;
               if ((t == cnt - 1) ^ dp->eof) continue;
+              if (w % 3 && w - x < sz && ((uint8_t *)dp->head)[w - x]) continue;
 
               uint32_t diff_down = rgb_down ? rgb_diff(rgb_down, (uint8_t *)dp->head) : 0;
               uint32_t diff_left = rgb_left ? rgb_diff(rgb_left, (uint8_t *)dp->head) : 0;
