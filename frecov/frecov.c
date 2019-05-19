666afea71c1ff3f4dca331f0357e03332506a311
tracer-nju <tracer@nju.edu.cn>
2019-05-20 00:20:45 +0800

    Static hostname: doowzs-vmbox          Icon name: computer-vm            Chassis: vm         Machine ID: 71088568831446748f7bb3a5439058a5            Boot ID: f2bd2ec33414455cb07431eaf74699b4     Virtualization: oracle   Operating System: Ubuntu 19.04             Kernel: Linux 5.0.0-13-generic       Architecture: x86-64  00:20:45 up 10:30,  6 users,  load average: 0.20, 0.32, 0.49

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -267,14 +267,6 @@ void handle_image(struct Image *image, size_t sz, int nr) {
         uint8_t j = ((uint8_t *)clus)[1] >> 4;
         uint8_t k = ((uint8_t *)clus)[2] >> 4;
 
-#ifdef DEBUG
-        Log("!");
-        ((uint8_t *)clus)[0] = 0xff;
-        ((uint8_t *)clus)[1] = 0x00;
-        ((uint8_t *)clus)[2] = 0x00;
-        Log("!");
-#endif
-
         struct DataSeg *dp = bmp_list[i][j][k].next;
         while (dp != &bmp_list[i][j][k] && dp->head != clus) dp = dp->next;
         if (dp != &bmp_list[i][j][k]) {
@@ -331,6 +323,9 @@ void handle_image(struct Image *image, size_t sz, int nr) {
         memcpy(ptr, clus, image->size - cnt * sz);
       } else {
         memcpy(ptr, clus, sz);
+#ifdef DEBUG
+        memset(ptr, 0xff, 3);
+#endif
         ptr += sz;
         clus += sz;
         x += sz;
