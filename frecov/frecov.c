1a4ac112ef9aa704e919115450f20e6b9a811621
tracer-nju <tracer@nju.edu.cn>
2019-05-19 23:09:30 +0800

    Static hostname: doowzs-vmbox          Icon name: computer-vm            Chassis: vm         Machine ID: 71088568831446748f7bb3a5439058a5            Boot ID: f2bd2ec33414455cb07431eaf74699b4     Virtualization: oracle   Operating System: Ubuntu 19.04             Kernel: Linux 5.0.0-13-generic       Architecture: x86-64  23:09:30 up  9:19,  6 users,  load average: 0.02, 0.24, 0.26

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -256,6 +256,8 @@ void handle_image(struct Image *image, size_t sz, int nr) {
       uint32_t diff_down = rgb_down ? rgb_diff(rgb_down, (uint8_t *)clus) : 0;
       uint32_t diff_left = rgb_left ? rgb_diff(rgb_left, (uint8_t *)clus) : 0;
       if (!strcmp(image->name, "./recov/fuli.bmp")) {
+        Log("pos x=%d, y=%d", (int)x, (int)y);
+        Log("rgb down=%p, left=%p", (void *)rgb_down, (void *)rgb_left);
         Log("diffs are %d %d", (int)diff_down, (int)diff_left);
       }
       sequent_ok = diff_down <= 300 && diff_left <= 300;
