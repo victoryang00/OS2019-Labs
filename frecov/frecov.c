e247131d7babfeef30d9a00d6ae300d45ba393c5
tracer-nju <tracer@nju.edu.cn>
2019-05-19 19:07:54 +0800

    Static hostname: doowzs-vmbox          Icon name: computer-vm            Chassis: vm         Machine ID: 71088568831446748f7bb3a5439058a5            Boot ID: f2bd2ec33414455cb07431eaf74699b4     Virtualization: oracle   Operating System: Ubuntu 19.04             Kernel: Linux 5.0.0-13-generic       Architecture: x86-64  19:07:55 up  5:17,  6 users,  load average: 0.92, 0.38, 0.34

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -215,6 +215,7 @@ void handle_image(struct Image *image, size_t sz) {
   CLog(FG_GREEN, "Start searching for %s, total size %d", image->name, (int) image->size);
 
   void *clus = disk->data + sz * (image->clus - disk->mbr->BPB_RootClus);
+  printf("first characters are %c%c\n", *(char *)clus, *((char *) clus + 1));
   image->width = *((int32_t *) clus + 0x12);
   image->height = *((int32_t *) clus + 0x16);
   Log("size: %d times %d", image->width, image->height);
