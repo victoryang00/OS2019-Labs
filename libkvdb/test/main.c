3ed07aadba7de2d32055e72ecf47e9761cd760bc
Tianyun Zhang <doowzs@qq.com>
2019-05-22 17:26:57 +0800

    update gitignore to ignore test files

--- libkvdb/test/main.c
+++ libkvdb/test/main.c
@@ -4,13 +4,13 @@
 int main() {
   kvdb_t db;
   int ret = 0;
-  const char *key = "operating-systems";
+  const char *key = "operating-system";
   char *value;
   
   ret = kvdb_open(&db, "a.db"); // BUG: should check for errors
   printf("ret is %d\n", ret);
 
-  ret = kvdb_put(&db, key, "three-easy-pieces");
+  ret = kvdb_put(&db, key, "thard-pieces");
   printf("ret is %d\n", ret);
 
   value = kvdb_get(&db, key);
