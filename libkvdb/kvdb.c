f7e34f8102f4b7adefff7cfe99c98b933b316db4
tracer-nju <tracer@nju.edu.cn>
2019-06-05 20:25:13 +0800

    Static hostname: doowzs-vmbox          Icon name: computer-vm            Chassis: vm         Machine ID: 71088568831446748f7bb3a5439058a5            Boot ID: f2bd2ec33414455cb07431eaf74699b4     Virtualization: oracle   Operating System: Ubuntu 19.04             Kernel: Linux 5.0.0-13-generic       Architecture: x86-64  20:25:14 up 12:10,  6 users,  load average: 0.25, 0.15, 0.16

--- libkvdb/kvdb.c
+++ libkvdb/kvdb.c
@@ -1,30 +1,123 @@
 #include "kvdb.h"
 
-int kvdb_open(kvdb_t *db, const char *filename) {
-  db->filename = filename;
-  db->fd = open(filename, O_CREAT | O_RDWR, S_IRWXU | S_IRWXG | S_IRWXO);
+inline void find_start(int fd) {
+  lseek(fd, SZ_RSVD, SEEK_SET);
+}
 
-  if (db->fd == -1) return -1;
+inline bool check_end(int fd) {
+  lseek(fd, 0, SEEK_END);
+  lseek(fd, -1, SEEK_CUR);
+  char buf;
+  read(fd, &buf, 1);
+  return buf == '\n';
+}
+
+inline void find_end(int fd) {
+  lseek(fd, 0, SEEK_END);
+  lseek(fd, -1, SEEK_CUR);
+  char buf;
+  read(fd, &buf, 1);
+  while (buf != '\n') {
+    lseek(fd, -2, SEEK_CUR);
+    read(fd, &buf, 1);
+  }
+}
+
+int kvdb_open(kvdb_t *db, const char *filename) {
+  if (db->fd != -1) return ER_ALRD;
+  db->filename = filename;
+  db->fd = open(filename, OP_TYPE, OP_PRIV);
+  if (db->fd == -1) return ER_OPEN;
   Log("%s opened", db->filename);
-  return 0;
+
+  off_t size = lseek(db->fd, 0, SEEK_END);
+  if ((size_t)size < SZ_RSVD) {
+    char *buf = malloc(SZ_RSVD);
+    buf[0] = buf[SZ_RSVD - 1] = '\n';
+    lseek(db->fd, 0, SEEK_SET);
+    write(db->fd, buf, SZ_RSVD);
+    lseek(db->fd, 0, SEEK_SET);
+    Log("%s initialized", db->filename);
+  }
+  return RT_SUCC;
 }
 
 int kvdb_close(kvdb_t *db) {
+  if (db->fd == -1) return ER_ALRD;
+  if (close(db->fd)) return ER_CLOS;
   Log("%s closed", db->filename);
-  return (close(db->fd)) ? -1 : 0;
+  return RT_SUCC;
+}
+
+void kvdb_fsck(kvdb_t *db) {
+  lseek(db->fd, 0, SEEK_SET);
+  char *buf = malloc(SZ_RSVD);
+  read(db->fd, buf, 1);
+  if (buf[0] != '\n') {
+    lseek(db->fd, 0, SEEK_SET);
+    read(db->fd, buf, SZ_RSVD);
+    char *key = malloc(SZ_KEYS);
+    char *val = malloc(SZ_VALV);
+    sscanf(buf, " %s %s", key, val);
+    
+    find_end(db->fd);
+    write(db->fd, key, strlen(key));
+    write(db->fd, " ", 1);
+    write(db->fd, val, strlen(val));
+    write(db->fd, "\n", 1);
+    // sync();
+    free(key);
+    free(val);
+
+    lseek(db->fd, 0, SEEK_SET);
+    write(db->fd, "\n", 1);
+  }
+  free(buf);
 }
 
 int kvdb_put(kvdb_t *db, const char *key, const char *value) {
+  if (flock(db->fd, LOCK_EX)) return ER_LOCK;
+  kvdb_fsck(db);
+  lseek(db->fd, 0, SEEK_SET);
+  write(db->fd, key, strlen(key));
+  write(db->fd, " ", 1);
+  write(db->fd, value, strlen(value));
+  write(db->fd, "\n", 1);
+  // sync();
+  kvdb_fsck(db);
+  if (flock(db->fd, LOCK_UN)) return ER_UNLK;
   return 0;
 }
 
 char *kvdb_get(kvdb_t *db, const char *key) {
-  char *value = NULL;
-  if (flock(db->fd, LOCK_SH)) return NULL;
-
-  if (flock(db->fd, LOCK_UN)) {
-    if (value) free(value);
+  char *buf = malloc(SZ_RSVD);
+  char *rkey = malloc(SZ_KEYS);
+  char *rval = malloc(SZ_VALV);
+  char *ret = malloc(SZ_VALV);
+  if (flock(db->fd, LOCK_EX)) {
+    free(buf);
+    free(rkey);
+    free(rval);
+    free(ret);
     return NULL;
-  };
-  return value;
+  }
+
+  kvdb_fsck(db);
+  find_start(db->fd);
+  off_t offset = lseek(db->fd, 0, SEEK_CUR);
+  while (read(db->fd, buf, sizeof(buf))) {
+    sscanf(buf, " %s %s", rkey, rval);
+    if (!strcmp(key, rkey)) strcpy(ret, rval);
+    offset += strlen(rkey) + strlen(rval) + 2;
+    lseek(db->fd, offset, SEEK_SET);
+  }
+
+  free(buf);
+  free(rkey);
+  free(rval);
+  if (flock(db->fd, LOCK_UN)) {
+    free(ret);
+    return NULL;
+  }
+  return ret;
 }
--- libkvdb/kvdb.h
+++ libkvdb/kvdb.h
@@ -24,6 +24,20 @@
 #define KB (1024 * Byte)
 #define MB (1024 * Byte)
 
+#define SZ_KEYS  1 * MB
+#define SZ_VALV 16 * MB
+#define SZ_RSVD 17 * MB
+
+#define OP_TYPE O_CREAT | O_RDWR
+#define OP_PRIV S_IRWXU | S_IRWXG | S_IRWXO
+
+#define ER_ALRD -1
+#define ER_OPEN -2
+#define ER_CLOS -3
+#define ER_LOCK -4
+#define ER_UNLK -5
+#define RT_SUCC 0
+
 struct kvdb {
   int fd;
   const char *filename;
@@ -32,6 +46,7 @@ typedef struct kvdb kvdb_t;
 
 int kvdb_open(kvdb_t *db, const char *filename);
 int kvdb_close(kvdb_t *db);
+void kvdb_fsck(kvdb_t *db);
 int kvdb_put(kvdb_t *db, const char *key, const char *value);
 char *kvdb_get(kvdb_t *db, const char *key);
 
