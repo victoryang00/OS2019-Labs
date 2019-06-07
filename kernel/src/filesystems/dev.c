4847210c19597ffed5703716ce3d4538eb461565
tracer-nju <tracer@nju.edu.cn>
2019-06-07 23:00:24 +0800

    Static hostname: doowzs-vmbox          Icon name: computer-vm            Chassis: vm         Machine ID: 71088568831446748f7bb3a5439058a5            Boot ID: 9f16cd2512f043b4af8d789703e5c054     Virtualization: oracle   Operating System: Ubuntu 19.04             Kernel: Linux 5.0.0-16-generic       Architecture: x86-64  23:00:24 up  7:47,  6 users,  load average: 0.36, 0.84, 0.65

--- kernel/src/filesystems/dev.c
+++ kernel/src/filesystems/dev.c
@@ -33,47 +33,16 @@ ssize_t devfs_write(file_t *file, const char *buf, size_t size) {
 
 void devfs_init(filesystem_t *fs, const char *path, device_t *dev) {
   Assert(!dev, "/dev has no device");
-  devfs_root.parent = &devfs_root;
+  devfs_root.parent = &root;
   devfs_root.fchild = NULL;
   devfs_root.cousin = NULL;
-  sprintf(devfs_root.path, "/");
+  sprintf(devfs_root.path, path);
   vfs->mount(path, fs);
 }
 
 inode_t *devfs_lookup(filesystem_t *fs, const char *path, int flags) {
   inode_t *ip = inode_search(&devfs_root, path);
-  if (!strcmp(path, ip->path)) return ip;
-  else {
-    CLog(FG_YELLOW, "inode %s not found. create a new one.", path);
-    device_t *device = dev_lookup(path + 1);
-    if (!device) {
-      CLog(FG_YELLOW, "device not found. creation failed.");
-      return NULL;
-    }
-
-    inode_t *dev_ip = pmm->alloc(sizeof(inode_t));
-    inodeops_t *ops = pmm->alloc(sizeof(inodeops_t));
-    ops->read = devfs_read;
-    ops->write = devfs_write;
-
-    dev_ip->refcnt = 0;
-    dev_ip->ptr = (void *)device;
-    sprintf(dev_ip->path, "%s", path);
-    dev_ip->fs = &devfs;
-    dev_ip->ops = ops;
-
-    dev_ip->parent = ip;
-    dev_ip->fchild = NULL;
-    dev_ip->cousin = NULL;
-    if (ip->fchild) {
-      ip = ip->fchild;
-      while (ip->cousin) ip = ip->cousin;
-      ip->cousin = dev_ip;
-    } else {
-      ip->fchild = dev_ip;
-    }
-    return dev_ip;
-  }
+  return (strlen(ip->path) == strlen(path)) ? ip : NULL;
 }
 
 int devfs_close(inode_t *inode) {
