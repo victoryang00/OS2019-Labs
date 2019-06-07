d6aa19dde6f4dcc0a5315dbdfec26a85e8f15424
tracer-nju <tracer@nju.edu.cn>
2019-06-07 15:39:27 +0800

    Static hostname: doowzs-vmbox          Icon name: computer-vm            Chassis: vm         Machine ID: 71088568831446748f7bb3a5439058a5            Boot ID: 9f16cd2512f043b4af8d789703e5c054     Virtualization: oracle   Operating System: Ubuntu 19.04             Kernel: Linux 5.0.0-16-generic       Architecture: x86-64  15:39:28 up 26 min,  5 users,  load average: 0.50, 0.37, 0.38

--- kernel/include/vfs.h
+++ kernel/include/vfs.h
@@ -6,16 +6,22 @@
 #include <klib.h>
 #include <devices.h>
 
-typedef struct fsops {
-  void (*init)(struct filesystem *fs, const char *name, device_t *dev);
-  inode_t *(*lookup)(struct filesystem *fs, const char *path, int flags);
-  int (*close)(inode_t *inode);
-} fsops_t;
+struct fsops;
+struct filesystem;
 
-typedef struct filesystem {
+typedef struct fsops fsops_t;
+typedef struct filesystem filesystem_t;
+
+struct fsops {
+  void (*init)(filesystem_t *fs, const char *name, device_t *dev);
+  inode_t *(*lookup)(filesystem_t *fs, const char *path, int flags);
+  int (*close)(inode_t *inode);
+};
+
+struct filesystem {
   fsops_t *ops;
   device_t *dev;
-} filesystem_t;
+};
 
 MODULE {
   void (*init)();
