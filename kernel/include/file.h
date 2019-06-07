c5ff1f88af2ea295c7bb470e9de9b734654fe80f
tracer-nju <tracer@nju.edu.cn>
2019-06-07 22:09:16 +0800

    Static hostname: doowzs-vmbox          Icon name: computer-vm            Chassis: vm         Machine ID: 71088568831446748f7bb3a5439058a5            Boot ID: 9f16cd2512f043b4af8d789703e5c054     Virtualization: oracle   Operating System: Ubuntu 19.04             Kernel: Linux 5.0.0-16-generic       Architecture: x86-64  22:09:16 up  6:56,  6 users,  load average: 0.79, 0.27, 0.27

--- kernel/include/file.h
+++ kernel/include/file.h
@@ -61,7 +61,7 @@ void common_readdir(inode_t *inode, char *ret);
 
 inline inode_t *inode_search(inode_t *cur, const char *path) {
   for (inode_t *ip = cur->fchild; ip != NULL; ip = ip->cousin) {
-    Log("%s", ip->path);
+    Log("%s -> %s", cur->path, ip->path);
     if (!strncmp(path, ip->path, strlen(ip->path))) {
       if (strlen(path) == strlen(ip->path)) return ip;
       else return inode_search(ip, path + strlen(ip->path));
