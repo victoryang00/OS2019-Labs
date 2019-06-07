32e9b3dfb8b32428e286f8ad9a62894121875f99
tracer-nju <tracer@nju.edu.cn>
2019-06-07 22:15:46 +0800

    Static hostname: doowzs-vmbox          Icon name: computer-vm            Chassis: vm         Machine ID: 71088568831446748f7bb3a5439058a5            Boot ID: 9f16cd2512f043b4af8d789703e5c054     Virtualization: oracle   Operating System: Ubuntu 19.04             Kernel: Linux 5.0.0-16-generic       Architecture: x86-64  22:15:46 up  7:02,  6 users,  load average: 2.31, 0.79, 0.44

--- kernel/src/filesystems/common.c
+++ kernel/src/filesystems/common.c
@@ -54,6 +54,7 @@ int common_unlink(const char *name) {
 void common_readdir(inode_t *inode, char *ret) {
   sprintf(ret, "ls %s:\n", inode->path);
   for (inode_t *ip = inode->fchild; ip != NULL; ip = ip->cousin) {
+    Log(ip->path);
     strcat(ret, " - ");
     strcat(ret, ip->path);
     strcat(ret, "\n");
