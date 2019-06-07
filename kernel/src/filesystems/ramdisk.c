a33f3c24aea0f409744b62c6efcb6a0ac55a8f3c
tracer-nju <tracer@nju.edu.cn>
2019-06-07 21:48:59 +0800

    Static hostname: doowzs-vmbox          Icon name: computer-vm            Chassis: vm         Machine ID: 71088568831446748f7bb3a5439058a5            Boot ID: 9f16cd2512f043b4af8d789703e5c054     Virtualization: oracle   Operating System: Ubuntu 19.04             Kernel: Linux 5.0.0-16-generic       Architecture: x86-64  21:48:59 up  6:36,  5 users,  load average: 0.32, 0.39, 0.65

--- kernel/src/filesystems/ramdisk.c
+++ kernel/src/filesystems/ramdisk.c
@@ -18,7 +18,7 @@ filesystem_t ramfs = {
 void ramfs_init(filesystem_t *fs, const char *path, device_t *dev) {
   sprintf(root.path, path);
   fs->dev = dev;
-  root->fs = fs;
+  root.fs = fs;
 }
 
 inode_t *ramfs_lookup(filesystem_t *fs, const char *path, int flags) {
