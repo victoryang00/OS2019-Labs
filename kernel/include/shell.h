818b17a51d81eb64d27fc610ab748b3830810270
tracer-nju <tracer@nju.edu.cn>
2019-06-07 19:19:54 +0800

    Static hostname: doowzs-vmbox          Icon name: computer-vm            Chassis: vm         Machine ID: 71088568831446748f7bb3a5439058a5            Boot ID: 9f16cd2512f043b4af8d789703e5c054     Virtualization: oracle   Operating System: Ubuntu 19.04             Kernel: Linux 5.0.0-16-generic       Architecture: x86-64  19:19:55 up  4:07,  5 users,  load average: 0.31, 0.33, 0.36

---
+++ kernel/include/shell.h
@@ -0,0 +1,25 @@
+#ifndef __SHELL_H__
+#define __SHELL_H__
+
+#include <common.h>
+
+typedef struct cmd {
+  const char name;
+  void (*func)(char *arg, char *ret);
+} cmd_t;
+
+void shell_task(void *arg);
+int handle_command(char *cmd);
+
+void ping(char *arg, char *ret);
+void fuck(char *arg, char *ret);
+void echo(char *arg, char *ret);
+
+void ls(char *arg, char *ret);
+void cd(char *arg, char *ret);
+void cat(char *arg, char *ret);
+void mkdir(char *arg, char *ret);
+void rmdir(char *arg, char *ret);
+void rm(char *arg, char *ret);
+
+#endif
--- kernel/src/shell.c
+++ kernel/src/shell.c
@@ -1,19 +1,46 @@
 #include <common.h>
 
+const cmd_t cmd_list[] = {
+  { "ping ",  ping  },
+  { "fuck ",  fuck  },
+  { "echo ",  echo  },
+  { "ls ",    ls    },
+  { "cd ",    cd    },
+  { "cat ",   cat   },
+  { "mkdir ", mkdir },
+  { "rmdir ", rmdir },
+  { "rm "   , rm    },
+};
+const int NR_CMD = sizeof(cmd_list) / sizeof(cmd_t);
+
 void shell_task(void *arg) {
   int tty_id = (int)arg;
   char buf[128] = "";
+  char cmd[256] = "";
+  char ret[256] = "";
 
   sprintf(buf, "/dev/tty%d", tty_id);
   int stdin = vfs->open(buf, O_RDONLY);
   int stdout = vfs->open(buf, O_WRONLY);
+
+  sprintf(buf, "(tty%d) $ ", tty_id);
   while (true) {
-    Log("before write");
-    sprintf(buf, "(tty%d) $ ", tty_id);
     vfs->write(stdout, buf, strlen(buf));
-    Log("before read");
-    ssize_t nread = vfs->read(stdin, buf, sizeof(buf));
-    vfs->write(stdout, buf, nread);
+    vfs->read(stdin, cmd, sizeof(cmd));
+
+    bool succ = false;
+    for (int i = 0; i < NR_CMD; ++i) {
+      if (!strcmp(buf, cmd_list[i].name)) {
+        succ = true;
+        char *arg = cmd + strlen(cmd_list[i].name);
+        cmd_list[i].func(arg, ret);
+      }
+    }
+    if (!succ) sprinf(ret, "invalid command.");
+    vfs->write(stdout, ret, strlen(ret));
+    vfs->write(stdout, "\n", 1);
   }
   Panic("shell cannot exit.");
 }
+
+
