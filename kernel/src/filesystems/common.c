9309333eec552538cf0f76b23575b900e765c793
tracer-nju <tracer@nju.edu.cn>
2019-06-09 00:27:23 +0800

    Static hostname: doowzs-vmbox          Icon name: computer-vm            Chassis: vm         Machine ID: 71088568831446748f7bb3a5439058a5            Boot ID: 9f16cd2512f043b4af8d789703e5c054     Virtualization: oracle   Operating System: Ubuntu 19.04             Kernel: Linux 5.0.0-16-generic       Architecture: x86-64  00:27:23 up 1 day,  9:14,  6 users,  load average: 0.41, 0.34, 0.27

--- kernel/src/filesystems/common.c
+++ kernel/src/filesystems/common.c
@@ -224,7 +224,7 @@ void mount_commonfs() {
 
 void commonfs_init(filesystem_t *fs, const char *path, device_t *dev) {
   fs->root->ptr = pmm->alloc(sizeof(commonfs_params_t));
-  size_t nread = dev->ops->read(dev, 0, fs->root->ptr, sizeof(commonfs_params_t));
+  dev->ops->read(dev, 0, fs->root->ptr, sizeof(commonfs_params_t));
   Log("read is %s", (char *)fs->root->ptr);
   int32_t blk = 1;
   while (blk) {
