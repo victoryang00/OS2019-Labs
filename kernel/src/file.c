17d74a21e637d01619d55073d029f06c988e9b86
tracer-nju <tracer@nju.edu.cn>
2019-06-08 21:16:11 +0800

    Static hostname: doowzs-vmbox          Icon name: computer-vm            Chassis: vm         Machine ID: 71088568831446748f7bb3a5439058a5            Boot ID: 9f16cd2512f043b4af8d789703e5c054     Virtualization: oracle   Operating System: Ubuntu 19.04             Kernel: Linux 5.0.0-16-generic       Architecture: x86-64  21:16:12 up 1 day,  6:03,  6 users,  load average: 0.38, 0.36, 0.31

---
+++ kernel/src/file.c
@@ -0,0 +1,42 @@
+#include <common.h>
+#include <file.h>
+
+inode_t *inode_search(inode_t *cur, const char *path) {
+  CLog(FG_BLUE, "looking for %s from %s", path, cur->path);
+  for (inode_t *ip = cur->fchild; ip != NULL; ip = ip->cousin) {
+    CLog(FG_BLUE, "%s -> %s", cur->path, ip->path);
+    if (!strncmp(path, ip->path, strlen(ip->path))) {
+      if (strlen(path) == strlen(ip->path)) return ip;
+      else return inode_search(ip, path);
+    }
+  }
+  return cur;
+}
+
+void inode_insert(inode_t *parent, inode_t *child) {
+  if (parent->fchild) {
+    inode_t *ip = parent->fchild;
+    while (ip->cousin) ip = ip->cousin;
+    ip->cousin = child;
+  } else {
+    parent->fchild = child;
+  }
+}
+
+void inode_remove(inode_t *parent, inode_t *child) {
+  if (parent->fchild == child) {
+    parent->fchild = child->cousin;
+  } else {
+    inode_t *ip = parent->fchild;
+    while (ip->cousin && ip->cousin != child) ip = ip->cousin;
+    if (ip->cousin == NULL) return;
+    ip->cousin = child->cousin;
+  }
+}
+
+void inode_delete(inode_t *cur) {
+  while (cur->fchild) {
+    inode_delete(cur->fchild);
+  }
+  inode_remove(cur->parent, cur);
+}
