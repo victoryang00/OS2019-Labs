c154285005d342bac11918ad52e50d08b0156177
tracer-nju <tracer@nju.edu.cn>
2019-06-09 17:50:59 +0800

    Static hostname: doowzs-vmbox          Icon name: computer-vm            Chassis: vm         Machine ID: 71088568831446748f7bb3a5439058a5            Boot ID: 9f16cd2512f043b4af8d789703e5c054     Virtualization: oracle   Operating System: Ubuntu 19.04             Kernel: Linux 5.0.0-16-generic       Architecture: x86-64  17:51:00 up 1 day, 15:04,  6 users,  load average: 0.11, 0.17, 0.29

--- kernel/src/vfs.c
+++ kernel/src/vfs.c
@@ -192,7 +192,10 @@ ssize_t vfs_read(int fd, void *buf, size_t nbyte) {
 ssize_t vfs_write(int fd, void *buf, size_t nbyte) {
   file_t *fp = find_file_by_fd(fd);
   Assert(fp, "file pointer is NULL");
-  Log("OKOK");
+  Log("fp: %p", fp);
+  Log("ip: %p", fp->inode);
+  Log("fs: %p", fp->inode->fs);
+  Log("fs: %s", fp->inode->fs->name);
   return fp->inode->ops->write(fp->inode->fs, fp, buf, nbyte);
 }
 
