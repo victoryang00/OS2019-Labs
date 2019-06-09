152c9d7bdfd575b8bd2f37974a150658dd425220
tracer-nju <tracer@nju.edu.cn>
2019-06-09 17:20:36 +0800

    Static hostname: doowzs-vmbox          Icon name: computer-vm            Chassis: vm         Machine ID: 71088568831446748f7bb3a5439058a5            Boot ID: 9f16cd2512f043b4af8d789703e5c054     Virtualization: oracle   Operating System: Ubuntu 19.04             Kernel: Linux 5.0.0-16-generic       Architecture: x86-64  17:20:36 up 1 day, 14:34,  6 users,  load average: 2.35, 0.77, 0.53

--- kernel/src/vfs.c
+++ kernel/src/vfs.c
@@ -97,7 +97,7 @@ int vfs_mount(const char *path, filesystem_t *fs) {
   inode_t *pp = inode_search(root, path);
   Assert(strcmp(path, pp->path), "inode %s exists!", path);
   fs->root->parent = pp;
-  inode_insert(pp, ip);
+  inode_insert(pp, fs->root);
   CLog(BG_YELLOW, "Path %s is mounted.", path);
   return 0;
 }
