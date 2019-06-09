d2948d1ef04819756e85fda0e0cece4116563f07
tracer-nju <tracer@nju.edu.cn>
2019-06-09 15:49:51 +0800

    Static hostname: doowzs-vmbox          Icon name: computer-vm            Chassis: vm         Machine ID: 71088568831446748f7bb3a5439058a5            Boot ID: 9f16cd2512f043b4af8d789703e5c054     Virtualization: oracle   Operating System: Ubuntu 19.04             Kernel: Linux 5.0.0-16-generic       Architecture: x86-64  15:49:52 up 1 day, 13:03,  6 users,  load average: 0.39, 0.43, 0.36

--- kernel/src/filesystems/naive.c
+++ kernel/src/filesystems/naive.c
@@ -169,7 +169,6 @@ ssize_t naive_read(filesystem_t *fs, file_t *file, char *buf, size_t size) {
 
 ssize_t naive_write(filesystem_t *fs, file_t *file, const char *buf, size_t size) {
   naivefs_params_t *params = (naivefs_params_t *)fs->root->ptr;
-      Log("blk size is %d", params->blk_size);
   off_t offset = file->inode->offset;
   int32_t blk = (int32_t)file->inode->ptr;
   
