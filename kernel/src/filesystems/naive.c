009d221d4b91f0d6d37bd6207fd6dcc76653459a
tracer-nju <tracer@nju.edu.cn>
2019-06-09 15:34:00 +0800

    Static hostname: doowzs-vmbox          Icon name: computer-vm            Chassis: vm         Machine ID: 71088568831446748f7bb3a5439058a5            Boot ID: 9f16cd2512f043b4af8d789703e5c054     Virtualization: oracle   Operating System: Ubuntu 19.04             Kernel: Linux 5.0.0-16-generic       Architecture: x86-64  15:34:01 up 1 day, 12:47,  6 users,  load average: 0.90, 0.54, 0.28

--- kernel/src/filesystems/naive.c
+++ kernel/src/filesystems/naive.c
@@ -388,7 +388,6 @@ inode_t *naivefs_lookup(filesystem_t *fs, const char *path, int flags) {
     }
   } else {
     if (flags & O_CREAT) {
-      Log("1");
       size_t len = strlen(path);
       if (len >= 23) return NULL; // naivefs limitation
       for (size_t i = strlen(ip->path) + 1; i < len; ++i) {
@@ -403,7 +402,6 @@ inode_t *naivefs_lookup(filesystem_t *fs, const char *path, int flags) {
       };
       ++params->min_free;
       int32_t blk = naivefs_add_entry(fs, &entry);
-      Log("2");
 
       inode_t *pp = ip;
       ip = pmm->alloc(sizeof(inode_t));
@@ -417,13 +415,11 @@ inode_t *naivefs_lookup(filesystem_t *fs, const char *path, int flags) {
       ip->fs = 0;
       ip->ops = pmm->alloc(sizeof(inodeops_t));
       memcpy(ip->ops, &naive_ops, sizeof(inodeops_t));
-      Log("3");
 
       ip->parent = pp;
       ip->fchild = NULL;
       ip->cousin = NULL;
       inode_insert(pp, ip);
-      Log("4");
       return ip;
     } else {
       return NULL;
