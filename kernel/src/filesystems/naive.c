179f1b3e21ea285c63ee8b5dc152e2202fcf5712
tracer-nju <tracer@nju.edu.cn>
2019-06-09 16:41:42 +0800

    Static hostname: doowzs-vmbox          Icon name: computer-vm            Chassis: vm         Machine ID: 71088568831446748f7bb3a5439058a5            Boot ID: 9f16cd2512f043b4af8d789703e5c054     Virtualization: oracle   Operating System: Ubuntu 19.04             Kernel: Linux 5.0.0-16-generic       Architecture: x86-64  16:41:43 up 1 day, 13:55,  5 users,  load average: 0.33, 0.39, 0.33

--- kernel/src/filesystems/naive.c
+++ kernel/src/filesystems/naive.c
@@ -284,7 +284,7 @@ int naive_rmdir(filesystem_t *fs, const char *path) {
 }
 
 int naive_link(filesystem_t *fs, const char *path, inode_t *inode) {
-  if (inode->type != TYPE_FILE) return -1;
+  if (inode->type != TYPE_FILE && inode->type != TYPE_LINK) return -1;
 
   inode_t *pp = inode_search(root, path);
   if (strlen(pp->path) == strlen(path)) return -2;
