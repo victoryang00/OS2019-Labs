74d133542b75c0fd02fe9fa49b98f46853df0df1
Tianyun Zhang <doowzs@qq.com>
2019-06-14 14:29:06 +0800

    bug fix: malloc for mutex

--- libkvdb/test/main.c
+++ libkvdb/test/main.c
@@ -7,26 +7,34 @@
 int main() {
   kvdb_t db;
   int ret = 0;
-  const char *key = "operating-systems";
-  char *value;
   
   Log(">>> Open");
-  ret = kvdb_open(&db, "a.db"); // BUG: should check for errors
+  ret = kvdb_open(&db, "a.db");
   Assert(!ret, "returned %d", ret);
 
-  Log(">>> Put");
-  ret = kvdb_put(&db, key, "nsnmndd");
-  Assert(!ret, "returned %d", ret);
-
-  Log(">>> Get");
-  value = kvdb_get(&db, key);
-  Assert(value, "returned NULL");
+  while (true) {
+    char cmd[16], key[128], val[128];
+    printf(">>> ");
+    scanf(" %s", cmd);
+    if (!strcmp(cmd, "set")) {
+      scanf(" %s %s", key, val);
+      kvdb_put(&db, key, val);
+    } else if (!strcmp(cmd, "get")) {
+      scanf(" %s", key);
+      char *ret = kvdb_get(&db, key);
+      if (ret) {
+        printf("<<< %s -> %s\n", key, ret);
+        free(ret);
+      } else {
+        printf("<<< %s not found\n", key);
+      }
+    } else {
+      printf("<<< invalid cmd\n");
+    }
+  }
 
   Log(">>> Close");
   ret = kvdb_close(&db);
   Assert(!ret, "returned %d", ret);
-
-  printf("[%s]: [%s]\n", key, value);
-  free(value);
   return 0;
 }
